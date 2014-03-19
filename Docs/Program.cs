f1c658054505ca847c619c7d2806f4a7383fef9b
Marc Gravell <marc.gravell@gmail.com>
2014-03-19 10:55:26 +0000

    Dummy to see if this is a viable way of handling man pages

---
+++ Docs/Program.cs
@@ -0,0 +1,4 @@
+﻿static class Program
+{
+    static void Main() { System.Console.WriteLine("This is a place-holder project just to make file management easier"); }
+}
---
+++ Docs/Transactions.md
@@ -0,0 +1,4 @@
+﻿Transactions in Redis
+=====================
+
+(dummy file to see if this works in github)
