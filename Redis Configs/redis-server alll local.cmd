666f3aea90b19b9a1ef7b9f759ac112ae8cddfc9
Marc Gravell <marc.gravell@gmail.com>
2014-03-19 10:05:02 +0000

    Fix "all local" servers cmd

--- Redis Configs/redis-server alll local.cmd
+++ Redis Configs/redis-server alll local.cmd
@@ -1,3 +1,3 @@
-@..\packages\Redis-64.2.6.12.1\tools\redis-server.exe master.conf
-@..\packages\Redis-64.2.6.12.1\tools\redis-server.exe slave.conf
-@..\packages\Redis-64.2.6.12.1\tools\redis-server.exe secure.conf
+@start ..\packages\Redis-64.2.6.12.1\tools\redis-server.exe master.conf
+@start ..\packages\Redis-64.2.6.12.1\tools\redis-server.exe slave.conf
+@start ..\packages\Redis-64.2.6.12.1\tools\redis-server.exe secure.conf
