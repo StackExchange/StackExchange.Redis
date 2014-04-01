588c921fc37c1be56801887c3e6a82cc39250238
Marc Gravell <marc.gravell@gmail.com>
2014-04-01 19:39:01 +0100

    SO22786599

---
+++ StackExchange.Redis.Tests/Issues/SO22786599.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+
+namespace StackExchange.Redis.Tests.Issues
+{
+    [TestFixture]
+    public class SO22786599 : TestBase
+    {
+        [Test]
+        public void Execute()
+        {
+            string CurrentIdsSetDbKey = Me() + ".x";
+            string CurrentDetailsSetDbKey = Me() + ".y";
+
+            RedisValue[] stringIds = Enumerable.Range(1, 750).Select(i => (RedisValue)(i + " id")).ToArray();
+            RedisValue[] stringDetails = Enumerable.Range(1, 750).Select(i => (RedisValue)(i + " detail")).ToArray();
+
+            using (var conn = Create())
+            {
+                var db = conn.GetDatabase();
+                var tran = db.CreateTransaction();
+
+                tran.SetAddAsync(CurrentIdsSetDbKey, stringIds);
+                tran.SetAddAsync(CurrentDetailsSetDbKey, stringDetails);
+
+                var watch = Stopwatch.StartNew();
+                var isOperationSuccessful = tran.Execute();
+                watch.Stop();
+                System.Console.WriteLine("{0}ms", watch.ElapsedMilliseconds);
+                Assert.IsTrue(isOperationSuccessful);                
+            }
+        }
+    }
+}
