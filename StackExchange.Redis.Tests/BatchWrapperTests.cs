24775ba38564f863793bc2e989fd29e8ec3f352a
mwikstrom <marten@marlin.nu>
2014-09-25 19:57:41 +0200

    Added 248 wrapper tests (key space isolation)

---
+++ StackExchange.Redis.Tests/BatchWrapperTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using StackExchange.Redis.StackExchange.Redis.KeyspaceIsolation;
+
+namespace StackExchange.Redis.Tests
+{
+    [TestFixture]
+    public sealed class BatchWrapperTests
+    {
+        private Mock<IBatch> mock;
+        private BatchWrapper wrapper;
+
+        [TestFixtureSetUp]
+        public void Initialize()
+        {
+            mock = new Mock<IBatch>();
+            wrapper = new BatchWrapper(mock.Object, "prefix:");
+        }
+
+        [Test]
+        public void Execute()
+        {
+            wrapper.Execute();
+            mock.Verify(_ => _.Execute(), Times.Once());
+        }
+    }
+}
