4626b120f7598c002eb8949592c5694146d6c5b6
olviko <olegwork@gmail.com>
2014-10-10 15:14:59 -0400

    HLL unit tests

---
+++ StackExchange.Redis.Tests/HyperLogLog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StackExchange.Redis.Tests
+{
+    [TestFixture]
+    public class HyperLogLog : TestBase
+    {
+        [Test]
+        public void SingleKeyLength()
+        {
+            using (var conn = Create())
+            {
+                var db = conn.GetDatabase();
+                RedisKey key = "hll1";
+
+                db.HyperLogLogAdd(key, "a");
+                db.HyperLogLogAdd(key, "b");
+                db.HyperLogLogAdd(key, "c");
+
+                Assert.IsTrue(db.HyperLogLogLength(key) > 0);
+            }
+        }
+
+        [Test]
+        public void MultiKeyLength()
+        {
+            using (var conn = Create(useSharedSocketManager: true))
+            {
+                var db = conn.GetDatabase();
+                RedisKey[] keys = { "hll1", "hll2", "hll3" };
+
+                db.HyperLogLogAdd(keys[0], "a");
+                db.HyperLogLogAdd(keys[1], "b");
+                db.HyperLogLogAdd(keys[2], "c");
+
+                Assert.IsTrue(db.HyperLogLogLength(keys) > 0);
+            }
+        }
+    }
+}
