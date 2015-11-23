9db8bbdf8cd5f232ab31327a68f52ec01adc2ef0
Jeremy Meng <yumeng@microsoft.com>
2015-11-23 11:59:29 -0800

    Link to the original author's code.

--- StackExchange.Redis/StackExchange/Redis/InternalRegexCompiledOption.cs
+++ StackExchange.Redis/StackExchange/Redis/InternalRegexCompiledOption.cs
@@ -5,6 +5,9 @@ using System.Text.RegularExpressions;
 
 namespace StackExchange.Redis
 {
+    /// <summary>
+    /// Credits to Sam Harwell https://github.com/dotnet/corefx/issues/340#issuecomment-120749951
+    /// </summary>
     internal static class InternalRegexCompiledOption
     {
         private static readonly RegexOptions RegexCompiledOption;
