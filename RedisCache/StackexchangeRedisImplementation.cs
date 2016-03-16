64b98fae54ed8213f952c1212c99fbef6fa10752
Michael Sejr Schlichtkrull <michael.sejr@gmail.com>
2016-03-16 14:11:19 +0100

    Hides internal redis interface

--- RedisCache/StackexchangeRedisImplementation.cs
+++ RedisCache/StackexchangeRedisImplementation.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RedisCache;
 using StackExchange.Redis;
 
 namespace RedisCache
 {
-    public class StackexchangeRedisImplementation :IRedisImplementation
+    internal class StackexchangeRedisImplementation :IRedisImplementation
     {
         private readonly ConnectionMultiplexer _connectionMultiplexer;
 
