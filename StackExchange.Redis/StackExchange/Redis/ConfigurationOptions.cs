abe6af2debe16952c629d93d892c249c40c9a7ae
Ian Oberst <areson@gmail.com>
2016-05-17 10:06:47 -0700

    Sentinel Support
    Initial pass at adding Sentinel support. Adds ConnectionMultiplexer that
    is managed by a set of Sentinel servers. Usage:
    ```C#
    ConfigurationOptions sentinelConfig = new ConfigurationOptions();
    sentinelConfig.ServiceName = "ServiceNameValue";
    sentinelConfig.EndPoints.Add("127.0.0.1", 26379);
    sentinelConfig.EndPoints.Add("127.0.0.2", 26379);
    sentinelConfig.EndPoints.Add("127.0.0.4", 26379);
    sentinelConfig.TieBreaker = "";
    sentinelConfig.DefaultVersion = new Version(3, 0);
    
    // Add the configuration for the masters that we will connect to
    ConfigurationOptions masterConfig = new ConfigurationOptions();
    sentinelConfig.SentinelMasterConfignfigurationOptions = mo;
    
    // Connect!
    ConnectionMultiplexer sentinelConnection =
    ConnectionMultiplexer.Connect(sentinelConfig);
    
    // When we need to perform a command against the masters, use the master
    connection
    sentinelConnection.SentinelMasterConnection.GetDatabase().MyCommand();
    ```

--- StackExchange.Redis/StackExchange/Redis/ConfigurationOptions.cs
+++ StackExchange.Redis/StackExchange/Redis/ConfigurationOptions.cs
@@ -105,7 +105,7 @@ namespace StackExchange.Redis
 
         private bool? allowAdmin, abortOnConnectFail, highPrioritySocketThreads, resolveDns, ssl;
 
-        private string clientName, serviceName, password, tieBreaker, sslHost, configChannel;
+        private string clientName, serviceName, password, tieBreaker, sslHost, configChannel;        
 
         private CommandMap commandMap;
 
@@ -113,7 +113,9 @@ namespace StackExchange.Redis
 
         private int? keepAlive, syncTimeout, connectTimeout, responseTimeout, writeBuffer, connectRetry, configCheckSeconds, defaultDatabase;
 
-        private Proxy? proxy;
+        private Proxy? proxy;        
+
+        private ConfigurationOptions sentinelMasterConfigurationOptions;
 
         /// <summary>
         /// A LocalCertificateSelectionCallback delegate responsible for selecting the certificate used for authentication; note
@@ -242,7 +244,12 @@ namespace StackExchange.Redis
         /// If enabled the ConnectionMultiplexer will not re-resolve DNS
         /// when attempting to re-connect after a connection failure.
         /// </summary>
-        public bool ResolveDns { get { return resolveDns.GetValueOrDefault(); } set { resolveDns = value; } }
+        public bool ResolveDns { get { return resolveDns.GetValueOrDefault(); } set { resolveDns = value; } }        
+        
+        /// <summary>
+        ///  Configuration options for the master that Sentinel connects to
+        /// </summary>
+        public ConfigurationOptions SentinelMasterConfigurationOptions { get { return sentinelMasterConfigurationOptions; } set { sentinelMasterConfigurationOptions = value; } }
 
         /// <summary>
         /// The service name used to resolve a service via sentinel
@@ -347,7 +354,8 @@ namespace StackExchange.Redis
                 ChannelPrefix = ChannelPrefix.Clone(),
                 SocketManager = SocketManager,
                 connectRetry = connectRetry,
-                configCheckSeconds = configCheckSeconds,
+                configCheckSeconds = configCheckSeconds,                
+                sentinelMasterConfigurationOptions = sentinelMasterConfigurationOptions,
                 responseTimeout = responseTimeout,
 				defaultDatabase = defaultDatabase,
             };
