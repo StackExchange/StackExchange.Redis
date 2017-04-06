3c5b5cca36df22eefbe13239e92506e7c0a0fb80
Marc Gravell <marc.gravell@gmail.com>
2017-04-06 11:24:15 +0100

    Add (broken) index.md; remove docs prog/cs

--- docs/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.0.0")]
-static class Program
-{
-    static void Main() { System.Console.WriteLine("This is a place-holder project just to make file management easier"); }
-}
