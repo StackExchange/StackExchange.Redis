9ad2a8e4abcc228cb27d794af63fb4abe83c7daa
Aleksandar Dragojević <rayalex@users.noreply.github.com>
2017-05-17 10:28:27 +0200

    Update mcs -> csc
    Project is using some C# 7 features which can only be compiled using CSC under mono 5.0

--- monobuild.bash
+++ monobuild.bash
@@ -3,9 +3,9 @@ rm -rf BasicTest/bin/mono
 mkdir -p StackExchange.Redis/bin/mono
 mkdir -p BasicTest/bin/mono
 echo -e "Building StackExchange.Redis.dll ..."
-mcs -recurse:StackExchange.Redis/*.cs -out:StackExchange.Redis/bin/mono/StackExchange.Redis.dll -target:library -unsafe+ -o+ -r:System.IO.Compression.dll
+csc -recurse:StackExchange.Redis/*.cs -out:StackExchange.Redis/bin/mono/StackExchange.Redis.dll -target:library -unsafe+ -o+ -r:System.IO.Compression.dll
 echo -e "Building BasicTest.exe ..."
-mcs BasicTest/Program.cs -out:BasicTest/bin/mono/BasicTest.exe -target:exe -o+ -r:StackExchange.Redis/bin/mono/StackExchange.Redis.dll
+csc BasicTest/Program.cs -out:BasicTest/bin/mono/BasicTest.exe -target:exe -o+ -r:StackExchange.Redis/bin/mono/StackExchange.Redis.dll
 cp StackExchange.Redis/bin/mono/*.* BasicTest/bin/mono/
 echo -e "Running basic test ..."
 mono BasicTest/bin/mono/BasicTest.exe 10000
