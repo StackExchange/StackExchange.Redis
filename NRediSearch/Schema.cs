e0343c188ad50a08abe2e1f4b29a5672a41e6421
rsubhank <rsubhank@visa.com>
2017-06-13 17:09:28 +0530

    Sorting Feature Added

--- NRediSearch/Schema.cs
+++ NRediSearch/Schema.cs
@@ -22,11 +22,13 @@ namespace NRediSearch
         {
             public String Name { get; }
             public FieldType Type { get; }
+            public bool Sortable {get;}
 
-            internal Field(string name, FieldType type)
+            internal Field(string name, FieldType type, bool sortable)
             {
                 Name = name;
                 Type = type;
+                Sortable = sortable;
             }
 
             internal virtual void SerializeRedisArgs(List<object> args)
@@ -43,12 +45,17 @@ namespace NRediSearch
                 }
                 args.Add(Name);
                 args.Add(GetForRedis(Type));
+                if(Sortable){args.Add("SORTABLE");}
             }
         }
         public class TextField : Field
         {
             public double Weight { get; }
-            internal TextField(string name, double weight = 1.0) : base(name, FieldType.FullText)
+            internal TextField(string name, double weight = 1.0) : base(name, FieldType.FullText, false)
+            {
+                Weight = weight;
+            }
+            internal TextField(string name, bool sortable, double weight = 1.0) : base(name, FieldType.FullText, sortable)
             {
                 Weight = weight;
             }
@@ -78,13 +85,25 @@ namespace NRediSearch
         }
 
         /// <summary>
+        /// Add a text field that can be sorted on
+        /// </summary>
+        /// <param name="name">the field's name</param>
+        /// <param name="weight">its weight, a positive floating point number</param>
+        /// <returns>the schema object</returns>
+        public Schema AddSortableTextField(string name, double weight = 1.0)
+        {
+            Fields.Add(new TextField(name, true, weight));
+            return this;
+        }
+
+        /// <summary>
         /// Add a numeric field to the schema
         /// </summary>
         /// <param name="name">the field's name</param>
         /// <returns>the schema object</returns>
         public Schema AddGeoField(string name)
         {
-            Fields.Add(new Field(name, FieldType.Geo));
+            Fields.Add(new Field(name, FieldType.Geo, false));
             return this;
         }
 
@@ -95,7 +114,18 @@ namespace NRediSearch
         /// <returns>the schema object</returns>
         public Schema AddNumericField(string name)
         {
-            Fields.Add(new Field(name, FieldType.Numeric));
+            Fields.Add(new Field(name, FieldType.Numeric, false));
+            return this;
+        }
+
+        /// <summary>
+        /// Add a numeric field that can be sorted on
+        /// </summary>
+        /// <param name="name">the field's name</param>
+        /// <returns>the schema object</returns>
+        public Schema AddSortableNumericField(string name)
+        {
+            Fields.Add(new Field(name, FieldType.Numeric, true));
             return this;
         }
 
