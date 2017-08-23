44a547625784948eb07b37febaeb5fa0c38fce94
Rufus <mail@rufuslinke.de>
2017-08-24 00:45:28 +0200

    use roslyn csc compiler on mono for C#7 support (#691)

--- monobuild.cmd
+++ monobuild.cmd
@@ -3,9 +3,9 @@
 @md StackExchange.Redis\bin\mono 1>nul 2>nul
 @md BasicTest\bin\mono 1>nul 2>nul
 @echo Building StackExchange.Redis.dll ...
-@call mcs -recurse:StackExchange.Redis\*.cs -out:StackExchange.Redis\bin\mono\StackExchange.Redis.dll -target:library -unsafe+ -o+ -r:System.IO.Compression.dll
+@call csc -recurse:StackExchange.Redis\*.cs -out:StackExchange.Redis\bin\mono\StackExchange.Redis.dll -target:library -unsafe+ -o+ -r:System.IO.Compression.dll
 @echo Building BasicTest.exe ...
-@call mcs BasicTest\Program.cs -out:BasicTest\bin\mono\BasicTest.exe -target:exe -o+ -r:StackExchange.Redis\bin\mono\StackExchange.Redis.dll
+@call csc BasicTest\Program.cs -out:BasicTest\bin\mono\BasicTest.exe -target:exe -o+ -r:StackExchange.Redis\bin\mono\StackExchange.Redis.dll
 @copy StackExchange.Redis\bin\mono\*.* BasicTest\bin\mono > nul
 @echo .
 @echo Running basic test (Mono) ...
