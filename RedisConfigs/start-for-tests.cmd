3926aca6146443acda81442f2baeb659767feec7
Nick Craver <nrcraver@gmail.com>
2017-08-30 22:11:13 -0400

    More sentinel config love

--- RedisConfigs/start-for-tests.cmd
+++ RedisConfigs/start-for-tests.cmd
@@ -4,4 +4,5 @@ echo Starting Redis servers on 6379, 6780, 6381, and 7000-7005
 @start "Redis (Slave): 6380" /min %~dp0\3.0.503\redis-server.exe %~dp0\slave.conf
 @start "Redis (Secure): 6381" /min %~dp0\3.0.503\redis-server.exe %~dp0\secure.conf
 call %~dp0\start-cluster.cmd
+call %~dp0\start-sentinel.cmd
 echo Servers started (minimized).
