e98b31579ea5b13b763ad62af9a4cc045d598f05
Nick Craver <nrcraver@gmail.com>
2017-08-30 22:03:06 -0400

    Initial Sentinel Configs
    These get overwritten when run, so they're ignored via:
    git update-index --assume-unchanged RedisConfigs\Sentinel\sentinel-26379.conf
    
    To make changes that should propagate to others:
    git update-index --no-assume-unchanged RedisConfigs\Sentinel\sentinel-26379.conf

---
+++ RedisConfigs/start-sentinel.cmd
@@ -0,0 +1,9 @@
+@echo off
+echo Starting Sentinel Targets: 7010-7011
+pushd %~dp0\Sentinel
+@start "Redis (Sentinel-Target): 7010" /min ..\3.0.503\redis-server.exe redis-7010.conf
+@start "Redis (Sentinel-Target): 7010" /min ..\3.0.503\redis-server.exe redis-7011.conf
+echo Starting Sentinel Monitors: 26379-26380
+@start "Redis (Sentinel): 26379" /min ..\3.0.503\redis-server.exe sentinel-26379.conf --sentinel
+@start "Redis (Sentinel): 26380" /min ..\3.0.503\redis-server.exe sentinel-26380.conf --sentinel
+popd
