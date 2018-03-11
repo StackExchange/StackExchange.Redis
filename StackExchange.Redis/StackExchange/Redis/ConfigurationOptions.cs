87960ea3d016d063c81d8db4bf0783001ecfdbe3
Nick Craver <nrcraver@gmail.com>
2018-03-11 14:46:05 -0400

    Cleanup: consolidate enums

--- StackExchange.Redis/StackExchange/Redis/ConfigurationOptions.cs
+++ StackExchange.Redis/StackExchange/Redis/ConfigurationOptions.cs
@@ -11,22 +11,6 @@ using System.Threading.Tasks;
 
 namespace StackExchange.Redis
 {
-
-    /// <summary>
-    /// Specifies the proxy that is being used to communicate to redis
-    /// </summary>
-    public enum Proxy
-    {
-        /// <summary>
-        /// Direct communication to the redis server(s)
-        /// </summary>
-        None,
-        /// <summary>
-        /// Communication via <a href="https://github.com/twitter/twemproxy">twemproxy</a>
-        /// </summary>
-        Twemproxy
-    }
-
     /// <summary>
     /// The options relevant to a set of redis connections
     /// </summary>
