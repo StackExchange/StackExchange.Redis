e6d810c4e392b82106b0256bf1c1c3674f0dcf76
Nick Craver <nrcraver@gmail.com>
2018-05-30 18:22:49 -0400

    Static initializers run in order and I'm an idiot.

--- StackExchange.Redis/StackExchange/Redis/PlatformHelper.cs
+++ StackExchange.Redis/StackExchange/Redis/PlatformHelper.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.IO.Compression;
 
 namespace StackExchange.Redis
 {
     internal class PlatformHelper
     {
-        public static SocketMode DefaultSocketMode => IsMono && IsUnix ? SocketMode.Async : SocketMode.Poll;
+        public static bool IsMono { get; } = Type.GetType("Mono.Runtime") != null;
+        public static bool IsUnix { get; } = (int)Environment.OSVersion.Platform == 4
+                                          || (int)Environment.OSVersion.Platform == 6
+                                          || (int)Environment.OSVersion.Platform == 128;
 
-        public static bool IsMono => Type.GetType("Mono.Runtime") != null;
-        public static bool IsUnix => (int)Environment.OSVersion.Platform == 4
-                                  || (int)Environment.OSVersion.Platform == 6
-                                  || (int)Environment.OSVersion.Platform == 128;
+        public static SocketMode DefaultSocketMode = IsMono && IsUnix ? SocketMode.Async : SocketMode.Poll;
     }
 }
