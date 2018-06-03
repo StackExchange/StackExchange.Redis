040f77d2b0e0a2c62d52d2452685eca89fc6d1ac
Nick Craver <nrcraver@gmail.com>
2018-06-03 09:38:15 -0400

    Tests: Guard text writes

--- StackExchange.Redis.Tests/Helpers/TextWriterOutputHelper.cs
+++ StackExchange.Redis.Tests/Helpers/TextWriterOutputHelper.cs
@@ -14,9 +14,18 @@ namespace StackExchange.Redis.Tests.Helpers
 
         public override void WriteLine(string value)
         {
-            base.Write(DateTime.UtcNow.ToString("HH:mm:ss.fff"));
-            base.Write(": ");
-            base.WriteLine(value);
+            try
+            {
+                base.Write(DateTime.UtcNow.ToString("HH:mm:ss.fff"));
+                base.Write(": ");
+                base.WriteLine(value);
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Attempted to write: ");
+                Console.WriteLine(value);
+                Console.WriteLine(ex);
+            }
         }
 
         public override void Write(char value)
