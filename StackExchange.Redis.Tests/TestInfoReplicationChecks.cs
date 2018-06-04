97d5969b783f6f691fb532c3f5c6210d6487ca75
Nick Craver <nrcraver@gmail.com>
2018-06-03 20:31:11 -0400

    Tests: shorten TestInfoReplicationChecks

--- StackExchange.Redis.Tests/TestInfoReplicationChecks.cs
+++ StackExchange.Redis.Tests/TestInfoReplicationChecks.cs
@@ -7,7 +7,7 @@ namespace StackExchange.Redis.Tests
 {
     public class TestInfoReplicationChecks : TestBase
     {
-        protected override string GetConfiguration() => base.GetConfiguration() + ",configCheckSeconds=5";
+        protected override string GetConfiguration() => base.GetConfiguration() + ",configCheckSeconds=2";
         public TestInfoReplicationChecks(ITestOutputHelper output) : base (output) { }
 
         [Fact]
@@ -16,9 +16,9 @@ namespace StackExchange.Redis.Tests
             using(var conn = Create())
             {
                 var parsed = ConfigurationOptions.Parse(conn.Configuration);
-                Assert.Equal(5, parsed.ConfigCheckSeconds);
+                Assert.Equal(2, parsed.ConfigCheckSeconds);
                 var before = conn.GetCounters();
-                Thread.Sleep(TimeSpan.FromSeconds(13));
+                Thread.Sleep(TimeSpan.FromSeconds(7));
                 var after = conn.GetCounters();
                 int done = (int)(after.Interactive.CompletedSynchronously - before.Interactive.CompletedSynchronously);
                 Assert.True(done >= 2);
