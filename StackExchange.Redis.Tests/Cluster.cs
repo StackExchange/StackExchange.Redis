c6f30d936a8e973589cdf1d1c3d7f7b91702cc3f
Nick Craver <nrcraver@gmail.com>
2018-06-09 21:53:57 -0400

    Just giving it a go
    I think there's a race in ReconfigureAsync with clusters here where the last subscription to finish is left out...but not seeing it yet. Tests show it over and over, though.

--- StackExchange.Redis.Tests/Cluster.cs
+++ StackExchange.Redis.Tests/Cluster.cs
@@ -46,7 +46,7 @@ namespace StackExchange.Redis.Tests
                     {
                         using (var muxer = Create(failMessage: i + ": ", log: sw))
                         {
-                            await Task.Delay(100);
+                            await Task.Delay(500);
                             foreach (var ep in muxer.GetEndPoints())
                             {
                                 var srv = muxer.GetServer(ep);
