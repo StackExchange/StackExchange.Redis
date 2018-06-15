7c709ecfd1f9ba92c77a263cce04a138ee00fd8c
Marc Gravell <marc.gravell@gmail.com>
2018-06-15 16:17:48 +0100

    use lib v5

--- BasicTest/Program.cs
+++ BasicTest/Program.cs
@@ -1,32 +1,40 @@
-﻿using StackExchange.Redis;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System;
-
-[assembly: AssemblyVersion("1.0.0")]
+﻿using System;
+using System.Threading.Tasks;
+using StackExchange.Redis;
 
 namespace BasicTest
 {
     internal static class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
-            using (var conn = ConnectionMultiplexer.Connect("127.0.0.1:6379"))
+            using (var conn = await ConnectionMultiplexer.ConnectAsync("127.0.0.1:6379"))
             {
-                var db = conn.GetDatabase();
+                var db = conn.GetDatabase(3);
 
-                db.KeyDelete("abc");
-                db.StringIncrement("abc");
-                db.StringIncrement("abc", 15);
-                db.StringIncrement("abc");
-                int i = (int)db.StringGet("abc");
-                Console.WriteLine(i);
+                var batch = db.CreateBatch();
+                var del = batch.KeyDeleteAsync("abc");
+                var set = batch.StringSetAsync("abc", "Does SE.Redis work on System.IO.Pipelines?");
+                var s = batch.StringGetAsync("abc");
+                batch.Execute();
+
+                await del;
+                await set;
+                Console.WriteLine(await s);
+
+                var rand = new Random(12345);
+                RedisKey counter = "counter";
+                db.KeyDelete(counter, CommandFlags.FireAndForget);
+                int expected = 0;
+                for (int i = 0; i < 1000; i++)
+                {
+                    int x = rand.Next(50);
+                    expected += x;
+                    db.StringIncrement(counter, x, CommandFlags.FireAndForget);
+                }
+                int actual = (int)db.StringGet(counter);
+                Console.WriteLine($"{expected} vs {actual}");
             }
-        }
-
-        internal static string Me([CallerMemberName] string caller = null)
-        {
-            return caller;
         }
     }
 }
