6e77b9c40ededb30f5480253867b851773531c36
Nick Craver <nrcraver@gmail.com>
2018-06-16 00:25:49 -0400

    Updates to GitVersioning + fixes build
    Note: this breaks Marc locally (by removing the package source), but if that Nuget.config is checked in, anyone lacking that directory will break, including our CI builds.

--- build.ps1
+++ build.ps1
@@ -1,49 +1,15 @@
 [CmdletBinding(PositionalBinding=$false)]
 param(
-    [string] $Version,
-    [string] $BuildNumber,
     [bool] $CreatePackages,
     [switch] $StartServers,
     [bool] $RunTests = $true,
     [string] $PullRequestNumber
 )
 
-function CalculateVersion() {
-    if ($Version) {
-        return $Version
-    }
-
-    $semVersion = '';
-    $path = $pwd;
-    while (!$semVersion) {
-        if (Test-Path (Join-Path $path "semver.txt")) {
-            $semVersion = Get-Content (Join-Path $path "semver.txt")
-            break
-        }
-        if ($PSScriptRoot -eq $path) {
-            break
-        }
-        $path = Split-Path $path -Parent
-    }
-
-    if (!$semVersion) {
-        Write-Error "semver.txt was not found in $pwd or any parent directory"
-        Exit 1
-    }
-
-    return "$semVersion-$BuildNumber"
-}
-
-if ($BuildNumber -and $BuildNumber.Length -lt 5) {
-    $BuildNumber = $BuildNumber.PadLeft(5, "0")
-}
-
 Write-Host "Run Parameters:" -ForegroundColor Cyan
-Write-Host "Version: $Version"
-Write-Host "BuildNumber: $BuildNumber"
-Write-Host "CreatePackages: $CreatePackages"
-Write-Host "RunTests: $RunTests"
-Write-Host "Base Version: $(CalculateVersion)"
+Write-Host "  CreatePackages: $CreatePackages"
+Write-Host "  RunTests: $RunTests"
+Write-Host "  dotnet --version:" (dotnet --version)
 
 $packageOutputFolder = "$PSScriptRoot\.nupkgs"
 $projectsToBuild =
@@ -52,17 +18,16 @@ $projectsToBuild =
 
 $testsToRun =
     'StackExchange.Redis.Tests'
-if (!$Version -and !$BuildNumber) {
-    Write-Host "ERROR: You must supply either a -Version or -BuildNumber argument. `
-  Use -Version `"4.0.0`" for explicit version specification, or `
-  Use -BuildNumber `"12345`" for generation using <semver.txt>-<buildnumber>" -ForegroundColor Yellow
-    Exit 1
-}
 
 if ($PullRequestNumber) {
     Write-Host "Building for a pull request (#$PullRequestNumber), skipping packaging." -ForegroundColor Yellow
     $CreatePackages = $false
 }
+
+Write-Host "Building solution..." -ForegroundColor "Magenta"
+dotnet restore ".\StackExchange.Redis.sln" /p:CI=true
+dotnet build ".\StackExchange.Redis.sln" -c Release /p:CI=true
+Write-Host "Done building." -ForegroundColor "Green"
 
 if ($RunTests) {
     if ($StartServers) {
@@ -95,31 +60,12 @@ if ($CreatePackages) {
     Write-Host "done." -ForegroundColor "Green"
 
     Write-Host "Building all packages" -ForegroundColor "Green"
-}
 
-foreach ($project in $projectsToBuild) {
-    Write-Host "Working on $project`:" -ForegroundColor "Magenta"
-
-    #Push-Location ".\src\$project"
-    Push-Location ".\$project"
-
-    $semVer = CalculateVersion
-    $targets = "Restore"
-
-    Write-Host "  Restoring " -NoNewline -ForegroundColor "Magenta"
-    if ($CreatePackages) {
-        $targets += ";Pack"
-		Write-Host "and packing " -NoNewline -ForegroundColor "Magenta"
+    foreach ($project in $projectsToBuild) {
+        Write-Host "Packing $project (dotnet pack)..." -ForegroundColor "Magenta"
+        dotnet pack ".\$project\$project.csproj" --no-build -c Release /p:PackageOutputPath=$packageOutputFolder /p:NoPackageAnalysis=true /p:CI=true
+        Write-Host ""
     }
-	Write-Host "$project... (Version:" -NoNewline -ForegroundColor "Magenta"
-    Write-Host $semVer -NoNewline -ForegroundColor "Cyan"
-    Write-Host ")" -ForegroundColor "Magenta"
-
-
-	dotnet msbuild "/t:$targets" "/p:Configuration=Release" "/p:Version=$semVer" "/p:PackageOutputPath=$packageOutputFolder" "/p:CI=true"
-
-	Pop-Location
-
-    Write-Host "Done."
-    Write-Host ""
 }
+
+Write-Host "Done."
