45e644c676dfa9d545d9696956844ccd1398a522
Nick Craver <nrcraver@gmail.com>
2018-06-16 00:25:59 -0400

    Last of the StrongName cleanup

--- StrongName.ps1
+++
@@ -1,3 +0,0 @@
-$key = Import-StrongNameKeyPair -KeyFile StackExchange.Redis.snk
-dir StackExchange.Redis*/bin/Release/StackExchange.Redis.dll | Set-StrongName -KeyPair $key -Verbose -NoBackup -Force
-nuget pack StackExchange.Redis.StrongName.nuspec
