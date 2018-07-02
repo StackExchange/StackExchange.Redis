158e1f2bbde732277f240ab72d01a02d28c517fd
Nick Craver <nrcraver@gmail.com>
2018-07-02 16:48:37 -0400

    Fix for #296
    Twemproxy handles this internally - we were incorrectly blocking it.

--- StackExchange.Redis/StackExchange/Redis/ConnectionMultiplexer.cs
+++ StackExchange.Redis/StackExchange/Redis/ConnectionMultiplexer.cs
@@ -1758,8 +1758,6 @@ namespace StackExchange.Redis
                 switch (server.ServerType)
                 {
                     case ServerType.Cluster:
-                    case ServerType.Twemproxy: // strictly speaking twemproxy uses a different hashing algo, but the hash-tag behavior is
-                                               // the same, so this does a pretty good job of spotting illegal commands before sending them
                         if (message.GetHashSlot(ServerSelectionStrategy) == ServerSelectionStrategy.MultipleSlots)
                         {
                             throw ExceptionFactory.MultiSlot(IncludeDetailInExceptions, message);
