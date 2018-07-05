33d286b1fd45dce7dc8b15d89c5bba073920e9df
Marc Gravell <marc.gravell@gmail.com>
2018-07-05 17:09:41 +0100

    implement `RedisValue.CreateFrom` to implement #866

--- StackExchange.Redis/StackExchange/Redis/RedisValue.cs
+++ StackExchange.Redis/StackExchange/Redis/RedisValue.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -741,6 +742,25 @@ namespace StackExchange.Redis
                     break;
             }
             return this;
+        }
+
+        /// <summary>
+        /// Create a RedisValue from a MemoryStream; it will *attempt* to use the internal buffer
+        /// directly, but if this isn't possibly it will fallback to ToArray
+        /// </summary>
+        public static RedisValue CreateFrom(MemoryStream stream)
+        {
+            if (stream == null) return Null;
+            if (stream.Length == 0) return Array.Empty<byte>();
+            if(stream.TryGetBuffer(out var segment))
+            {
+                return new Memory<byte>(segment.Array, segment.Offset, segment.Count);
+            }
+            else
+            {
+                // nowhere near as efficient, but...
+                return stream.ToArray();
+            }
         }
     }
 }
