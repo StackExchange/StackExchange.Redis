48fb5afbaeaf5cbb2f940490afd67756038eed66
Marc Gravell <marc.gravell@gmail.com>
2018-07-23 14:53:45 +0100

    add readme to the server code

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,131 +1,16 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
 using System.Net;
-using StackExchange.Redis;
-using StackExchange.Redis.Server;
-using System.Runtime.Caching;
-using System.Threading;
 using System.Threading.Tasks;
-using System.Collections.Generic;
+using StackExchange.Redis.Server;
 
-namespace TestConsole
+internal static class Program
 {
-    internal static class Program
+    private static async Task Main()
     {
-        class FakeRedisServer : RedisServer
+        using (var server = new MemoryCacheServer(Console.Out))
         {
-            public FakeRedisServer(TextWriter output = null) : base(1, output)
-                => CreateNewCache();
-
-            private MemoryCache _cache;
-
-            private void CreateNewCache()
-            {
-                var old = _cache;
-                _cache = new MemoryCache(GetType().Name);
-                if (old != null) old.Dispose();
-            }
-
-            protected override void Dispose(bool disposing)
-            {
-                if (disposing) _cache.Dispose();
-                base.Dispose(disposing);
-            }
-
-            protected override long Dbsize(int database) => _cache.GetCount();
-            protected override RedisValue Get(int database, RedisKey key)
-                => RedisValue.Unbox(_cache[key]);
-            protected override void Set(int database, RedisKey key, RedisValue value)
-                => _cache[key] = value.Box();
-            protected override bool Del(int database, RedisKey key)
-                => _cache.Remove(key) != null;
-            protected override void Flushdb(int database)
-                => CreateNewCache();
-
-            protected override bool Exists(int database, RedisKey key)
-                => _cache.Contains(key);
-
-            protected override IEnumerable<RedisKey> Keys(int database, RedisKey pattern)
-            {
-                string s = pattern;
-                foreach (var pair in _cache)
-                {
-                    if (IsMatch(pattern, pair.Key)) yield return pair.Key;
-                }
-            }
-            protected override bool Sadd(int database, RedisKey key, RedisValue value)
-                => GetSet(key, true).Add(value);
-
-            protected override bool Sismember(int database, RedisKey key, RedisValue value)
-                => GetSet(key, false)?.Contains(value) ?? false;
-
-            protected override bool Srem(int database, RedisKey key, RedisValue value)
-            {
-                var set = GetSet(key, false);
-                if (set != null && set.Remove(value))
-                {
-                    if (set.Count == 0) _cache.Remove(key);
-                    return true;
-                }
-                return false;
-            }
-            protected override long Scard(int database, RedisKey key)
-                => GetSet(key, false)?.Count ?? 0;
-
-            HashSet<RedisValue> GetSet(RedisKey key, bool create)
-            {
-                var set = (HashSet<RedisValue>)_cache[key];
-                if (set == null && create)
-                {
-                    set = new HashSet<RedisValue>();
-                    _cache[key] = set;
-                }
-                return set;
-            }
-
-        }
-        private static async Task Main()
-        {
-            long oldOps = 0;
-            var ep = new IPEndPoint(IPAddress.Loopback, 6378);
-            using (var server = new FakeRedisServer(Console.Out))
-            using (var timer = new Timer(_ =>
-            {
-                var ops = server.CommandsProcesed;
-                if (oldOps != ops)
-                {
-                    lock (Console.Out)
-                    {
-                        Console.WriteLine($"Commands processed: " + ops);
-                    }
-                    oldOps = ops;
-                }
-            }, null, 1000, 1000))
-            {
-                server.Listen(ep);
-                TimeClient(ep);
-                await server.Shutdown;
-            }
-        }
-
-        static void TimeClient(EndPoint ep)
-        {
-            Console.WriteLine("testing server...");
-            var cfg = new ConfigurationOptions { EndPoints = { ep } };
-            using (var client = ConnectionMultiplexer.Connect(cfg))
-            {
-                var db = client.GetDatabase();
-                var watch = Stopwatch.StartNew();
-                const int LOOP = 1000;
-                for (int i = 0; i < LOOP; i++)
-                {
-                    db.Ping();
-                }
-                watch.Stop();
-
-                Console.WriteLine($"ping {LOOP} times: {watch.ElapsedMilliseconds}ms");
-            }
+            server.Listen(new IPEndPoint(IPAddress.Loopback, 6378));
+            await server.Shutdown;
         }
     }
 }
