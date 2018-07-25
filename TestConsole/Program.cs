d6d4683a63d5297fced4b748cf20caa28c3bef5b
Marc Gravell <marc.gravell@gmail.com>
2018-07-25 09:38:50 +0100

    save *all* the files!

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -8,14 +8,14 @@ static class Program
 {
     static async Task Main()
     {
-        using (var pool = new Pipelines.Sockets.Unofficial.DedicatedThreadPoolPipeScheduler(minWorkers: 10, maxWorkers: 50,
-            priority: System.Threading.ThreadPriority.Highest))
+        //using (var pool = new Pipelines.Sockets.Unofficial.DedicatedThreadPoolPipeScheduler(minWorkers: 10, maxWorkers: 50,
+        //    priority: System.Threading.ThreadPriority.Highest))
         using (var resp = new MemoryCacheRedisServer(Console.Out))
         using (var socket = new RespSocketServer(resp))
         {
-            var options = new PipeOptions(readerScheduler: pool, writerScheduler: pool, useSynchronizationContext: false);
+            //var options = new PipeOptions(readerScheduler: pool, writerScheduler: pool, useSynchronizationContext: false);
             socket.Listen(new IPEndPoint(IPAddress.Loopback, 6378)
-                , sendOptions: options, receiveOptions: options
+            //    , sendOptions: options, receiveOptions: options
             );
             await resp.Shutdown;
         }
