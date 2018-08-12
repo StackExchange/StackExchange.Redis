a951728feabbe4efa94f085af1dc3e7c7b58848f
Nick Craver <nrcraver@gmail.com>
2018-08-11 21:56:21 -0400

    More line ending love

--- StackExchange.Redis.Tests/RedisFeaturesTests.cs
+++ StackExchange.Redis.Tests/RedisFeaturesTests.cs
@@ -11,20 +11,20 @@ namespace StackExchange.Redis.Tests
             var features = new RedisFeatures(new Version(2, 9));
             var s = features.ToString();
             Assert.True(features.ExecAbort);
-            Assert.StartsWith("Features in 2.9\r\n", s);
-            Assert.Contains("ExecAbort: True\r\n", s);
+            Assert.StartsWith("Features in 2.9" + Environment.NewLine, s);
+            Assert.Contains("ExecAbort: True" + Environment.NewLine, s);
 
             features = new RedisFeatures(new Version(2, 9, 5));
             s = features.ToString();
             Assert.False(features.ExecAbort);
-            Assert.StartsWith("Features in 2.9.5\r\n", s);
-            Assert.Contains("ExecAbort: False\r\n", s);
+            Assert.StartsWith("Features in 2.9.5" + Environment.NewLine, s);
+            Assert.Contains("ExecAbort: False" + Environment.NewLine, s);
 
             features = new RedisFeatures(new Version(3, 0));
             s = features.ToString();
             Assert.True(features.ExecAbort);
-            Assert.StartsWith("Features in 3.0\r\n", s);
-            Assert.Contains("ExecAbort: True\r\n", s);
+            Assert.StartsWith("Features in 3.0" + Environment.NewLine, s);
+            Assert.Contains("ExecAbort: True" + Environment.NewLine, s);
         }
     }
 }
