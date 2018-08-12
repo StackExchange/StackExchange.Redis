9d4dc23d98f4f15412e8eb0d17e8fe088078e436
Nick Craver <nrcraver@gmail.com>
2018-08-11 20:44:15 -0400

    Bash: fix indents
    Inadvertent revert in a merge I think

--- RedisConfigs/start-all.sh
+++ RedisConfigs/start-all.sh
@@ -1,22 +1,23 @@
-echo Starting Redis servers for testing...
+INDENT='   '
+echo "Starting Redis servers for testing..."
 
 #Basic Servers
-echo Starting Basic: 6379-6382
+echo "Starting Basic: 6379-6382"
 pushd Basic > /dev/null
-echo   Master: 6379
+echo "${INDENT}Master: 6379"
 redis-server master-6379.conf &>/dev/null &
-echo   Slave: 6380
+echo "${INDENT}Slave: 6380"
 redis-server slave-6380.conf &>/dev/null &
-echo   Secure: 6381
+echo "${INDENT}Secure: 6381"
 redis-server secure-6381.conf &>/dev/null &
 popd > /dev/null
 
 #Failover Servers
 echo Starting Failover: 6382-6383
 pushd Failover > /dev/null
-echo   Master: 6382
+echo "${INDENT}Master: 6382"
 redis-server master-6382.conf &>/dev/null &
-echo   Slave: 6383
+echo "${INDENT}Slave: 6383"
 redis-server slave-6383.conf &>/dev/null &
 popd > /dev/null
 
@@ -34,10 +35,10 @@ popd > /dev/null
 #Sentinel Servers
 echo Starting Sentinel: 7010-7011,26379-26380
 pushd Sentinel > /dev/null
-echo   Targets: 7010-7011
+echo "${INDENT}Targets: 7010-7011"
 redis-server redis-7010.conf &>/dev/null &
 redis-server redis-7011.conf &>/dev/null &
-echo   Monitors: 26379-26380
+echo "${INDENT}Monitors: 26379-26380"
 redis-server sentinel-26379.conf --sentinel &>/dev/null &
 redis-server sentinel-26380.conf --sentinel &>/dev/null &
 popd > /dev/null
