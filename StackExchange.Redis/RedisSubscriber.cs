d86aa4ca044914647838432e832cfd8869727175
Marc Gravell <marc.gravell@gmail.com>
2018-08-15 16:17:22 +0100

    Conditionalize a bunch of things

--- StackExchange.Redis/RedisSubscriber.cs
+++ StackExchange.Redis/RedisSubscriber.cs
@@ -258,35 +258,37 @@ namespace StackExchange.Redis
         internal event Action<EndPoint, ConnectionType> Connecting;
         internal event Action<EndPoint, ConnectionType> Resurrecting;
 
+        [Conditional("VERBOSE")]
         internal void OnMessageFaulted(Message msg, Exception fault, [CallerMemberName] string origin = default, [CallerFilePath] string path = default, [CallerLineNumber] int lineNumber = default)
         {
             MessageFaulted?.Invoke(msg?.CommandAndKey, fault, $"{origin} ({path}#{lineNumber})");
         }
+        [Conditional("VERBOSE")]
         internal void OnInfoMessage(string message)
         {
             InfoMessage?.Invoke(message);
         }
-
+        [Conditional("VERBOSE")]
         internal void OnClosing(bool complete)
         {
             Closing?.Invoke(complete);
         }
-
+        [Conditional("VERBOSE")]
         internal void OnConnecting(EndPoint endpoint, ConnectionType connectionType)
         {
             Connecting?.Invoke(endpoint, connectionType);
         }
-
+        [Conditional("VERBOSE")]
         internal void OnResurrecting(EndPoint endpoint, ConnectionType connectionType)
         {
             Resurrecting.Invoke(endpoint, connectionType);
         }
-
+        [Conditional("VERBOSE")]
         internal void OnPreTransactionExec(Message message)
         {
             PreTransactionExec?.Invoke(message.CommandAndKey);
         }
-
+        [Conditional("VERBOSE")]
         internal void OnTransactionLog(string message)
         {
             TransactionLog?.Invoke(message);
