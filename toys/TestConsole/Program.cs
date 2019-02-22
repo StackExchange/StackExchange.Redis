0ad5c405f1d8a86f7aa86137dc80904f046c67a8
Marc Gravell <marc.gravell@gmail.com>
2019-02-22 10:26:45 +0000

    use the perf test added recently; rev lib

--- toys/TestConsole/Program.cs
+++ toys/TestConsole/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using StackExchange.Redis;
 
 namespace TestConsole
 {
     internal static class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
+            var client = ConnectionMultiplexer.Connect("localhost");
+            client.GetDatabase().Ping();
+            var db = client.GetDatabase(0);
+
+            var start = DateTime.Now;
+
+            Show(client.GetCounters());
+            var tasks = Enumerable.Range(0, 1000).Select(async i =>
+            {
+                for (int t = 0; t < 1000; t++)
+                {
+                    await db.StringIncrementAsync(i.ToString(), 1);
+                    // db.StringIncrement(i.ToString(), 1);
+                }
+                await Task.Yield();
+            });
+
+            await Task.WhenAll(tasks);
+            Show(client.GetCounters());
+
+            var duration = DateTime.Now.Subtract(start).TotalMilliseconds;
+            Console.WriteLine($"{duration}ms");
+        }
+        private static void Show(ServerCounters counters)
+        {
+            Console.WriteLine("CA: " + counters.Interactive.CompletedAsynchronously);
+            Console.WriteLine("FA: " + counters.Interactive.FailedAsynchronously);
+            Console.WriteLine("CS: " + counters.Interactive.CompletedSynchronously);
+            Console.WriteLine();
         }
     }
 }
