f7f87679a59df40f89c6cd8171b79fe98499a6db
mgravell <marc.gravell@gmail.com>
2019-03-29 10:35:18 +0000

    add failing test for #1103

---
+++ tests/StackExchange.Redis.Tests/Issues/Issue1103.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace StackExchange.Redis.Tests.Issues
+{
+    public class Issue1103 : TestBase
+    {
+        public Issue1103(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public void LargeUInt64StoredCorrectly()
+        {
+            using (var muxer = Create())
+            {
+                var db = muxer.GetDatabase();
+                const ulong expected = 142205255210238005;
+                db.StringSet("foo", expected);
+                var val = db.StringGet("foo");
+                Log((string)val);
+                var actual = (ulong)val;
+                Assert.Equal(expected, actual);
+            }
+        }
+    }
+}
