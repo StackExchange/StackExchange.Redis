4eb817fd594d3154ab6f31cee8053a4620cc974f
Tom Hanks <tommy.hanks@gmail.com>
2019-11-01 05:48:13 -0400

    Added Load, Limit, and SortBy

--- src/NRediSearch/Aggregation/AggregationBuilder.cs
+++ src/NRediSearch/Aggregation/AggregationBuilder.cs
@@ -6,7 +6,7 @@ namespace NRediSearch.Aggregation
 {
     public class AggregationBuilder
     {
-        private readonly List<string> args = new List<string>();
+        private readonly List<object> args = new List<object>();
 
         public AggregationBuilder() : this("*")
         {
@@ -14,13 +14,45 @@ namespace NRediSearch.Aggregation
 
         public AggregationBuilder(string query) => args.Add(query);
 
-        private static void AddCommandLength(List<string> list, string command, int length)
+        public AggregationBuilder Load(params string[] fields)
+        {
+            AddCommandArguments(args, "LOAD", fields);
+
+            return this;
+        }
+
+        public AggregationBuilder Limit(int offset, int count)
+        {
+            var limit = new Limit(offset, count);
+
+            limit.SerializeRedisArgs(args);
+
+            return this;
+        }
+
+        public AggregationBuilder Limit(int count) => Limit(0, count);
+
+        public AggregationBuilder SortBy(params SortedField[] fields)
+        {
+            args.Add("SORTBY");
+            args.Add((fields.Length * 2).ToString());
+
+            foreach (var field in fields)
+            {
+                args.Add(field.Field);
+                args.Add(field.Order);
+            }
+
+            return this;
+        }
+
+        private static void AddCommandLength(List<object> list, string command, int length)
         {
             list.Add(command);
             list.Add(length.ToString());
         }
 
-        private static void AddCommandArguments(List<string> destination, string command, List<string> source)
+        private static void AddCommandArguments(List<object> destination, string command, IReadOnlyCollection<object> source)
         {
             AddCommandLength(destination, command, source.Count);
             destination.AddRange(source);
