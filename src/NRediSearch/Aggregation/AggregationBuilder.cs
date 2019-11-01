7e7f64682158f179737c345b47d5553960d83835
Tom Hanks <tommy.hanks@gmail.com>
2019-11-01 05:27:40 -0400

    Ported in static support methods.

--- src/NRediSearch/Aggregation/AggregationBuilder.cs
+++ src/NRediSearch/Aggregation/AggregationBuilder.cs
@@ -1,6 +1,29 @@
-﻿namespace NRediSearch.Aggregation
+﻿// .NET port of https://github.com/RedisLabs/JRediSearch/
+
+using System.Collections.Generic;
+
+namespace NRediSearch.Aggregation
 {
     public class AggregationBuilder
     {
+        private readonly List<string> args = new List<string>();
+
+        public AggregationBuilder() : this("*")
+        {
+        }
+
+        public AggregationBuilder(string query) => args.Add(query);
+
+        private static void AddCommandLength(List<string> list, string command, int length)
+        {
+            list.Add(command);
+            list.Add(length.ToString());
+        }
+
+        private static void AddCommandArguments(List<string> destination, string command, List<string> source)
+        {
+            AddCommandLength(destination, command, source.Count);
+            destination.AddRange(source);
+        }
     }
 }
