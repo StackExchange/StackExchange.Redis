e304db4cac1488de6cbc3df1609f6f047a6e66e8
Tom Hanks <tommy.hanks@gmail.com>
2019-11-02 06:54:49 -0400

    Ported SuggestionOptions.

---
+++ src/NRediSearch/SuggestionOptions.cs
@@ -0,0 +1,70 @@
+﻿// .NET port of https://github.com/RedisLabs/JRediSearch/
+
+namespace NRediSearch
+{
+    public class SuggestionOptions
+    {
+        private const string WITHPAYLOADS_FLAG = "WITHPAYLOADS";
+        private const string WITHSCORES_FLAG = "WITHSCORES";
+
+        public SuggestionOptions(Builder builder)
+        {
+            _with = builder._with;
+            Fuzzy = builder._fuzzy;
+            Max = builder._max;
+        }
+
+        public static Builder GetBuilder() => new Builder();
+
+        private With _with;
+        public With GetWith() => _with;
+
+        public bool Fuzzy { get; }
+
+        public int Max { get; }
+
+        public Builder ToBuilder() => new Builder(this);
+
+        public class With
+        {
+            public static With PAYLOAD = new With(WITHPAYLOADS_FLAG);
+            public static With SCORES = new With(WITHSCORES_FLAG);
+            public static With PAYLOAD_AND_SCORES = new With(WITHPAYLOADS_FLAG, WITHSCORES_FLAG);
+
+            public string[] Flags { get; }
+
+            private With(params string[] flags) => Flags = flags;
+        }
+
+        public sealed class Builder
+        {
+            internal With _with;
+            internal bool _fuzzy;
+            internal int _max;
+
+            public Builder() { }
+
+            public Builder(SuggestionOptions options)
+            {
+                _with = options.GetWith();
+                _fuzzy = options.Fuzzy;
+                _max = options.Max;
+            }
+
+            public Builder Fuzzy()
+            {
+                return this;
+            }
+
+            public Builder Max(int max)
+            {
+                return this;
+            }
+
+            public SuggestionOptions Build()
+            {
+                return new SuggestionOptions(this);
+            }
+        }
+    }
+}
