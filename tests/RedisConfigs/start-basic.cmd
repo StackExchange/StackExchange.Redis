5050cfb8822ba43013b02305bc7d5524d095d9fd
Nick Craver <craver@stackoverflow.com>
2020-01-06 18:53:46 -0500

    Testing a Redis 4.0 release
    From https://github.com/tporadowski/redis - let's give it a test run spin just to check things out :)

--- tests/RedisConfigs/start-basic.cmd
+++ tests/RedisConfigs/start-basic.cmd
@@ -2,9 +2,9 @@
 echo Starting Basic: 
 pushd %~dp0\Basic
 echo   Master: 6379
-@start "Redis (Master): 6379" /min ..\3.0.503\redis-server.exe master-6379.conf
+@start "Redis (Master): 6379" /min ..\4.0.14\redis-server.exe master-6379.conf
 echo   Slave: 6380
-@start "Redis (Slave): 6380" /min ..\3.0.503\redis-server.exe slave-6380.conf
+@start "Redis (Slave): 6380" /min ..\4.0.14\redis-server.exe slave-6380.conf
 echo   Secure: 6381
-@start "Redis (Secure): 6381" /min ..\3.0.503\redis-server.exe secure-6381.conf
+@start "Redis (Secure): 6381" /min ..\4.0.14\redis-server.exe secure-6381.conf
 popd
