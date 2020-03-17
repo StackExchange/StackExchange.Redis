5b7bef288181a6b22adb0454aa496d87088547fd
Nick Craver <craver@stackoverflow.com>
2020-03-16 21:22:35 -0400

    A few fixes for the docker build
    - Moves to the non-alpine image
      - This fixes module loading, like NRediSearch
    - Removes directory nesting for files (not needed)
    - Restores start-all.sh
      - With 23681

--- tests/RedisConfigs/Docker/docker-entrypoint.sh
+++ tests/RedisConfigs/Docker/docker-entrypoint.sh
@@ -3,25 +3,6 @@
 if [ "$#" -ne 0 ]; then
     exec "$@"
 else
-    mkdir -p /data/Temp/master-6379
-    mkdir -p /data/Temp/secure-6381
-    mkdir -p /data/Temp/slave-6380
-    mkdir -p /data/Temp/master-6382
-    mkdir -p /data/Temp/slave-6383
-
-    mkdir -p /data/Temp/cluster-7000
-    mkdir -p /data/Temp/cluster-7001
-    mkdir -p /data/Temp/cluster-7002
-    mkdir -p /data/Temp/cluster-7003
-    mkdir -p /data/Temp/cluster-7004
-    mkdir -p /data/Temp/cluster-7005
-
-    mkdir -p /data/Temp/redis-7010
-    mkdir -p /data/Temp/redis-7011
-    mkdir -p /data/Temp/sentinel-26379
-    mkdir -p /data/Temp/sentinel-26380
-    mkdir -p /data/Temp/sentinel-26381
-
     mkdir -p /var/log/supervisor
 
     supervisord -c /etc/supervisord.conf
