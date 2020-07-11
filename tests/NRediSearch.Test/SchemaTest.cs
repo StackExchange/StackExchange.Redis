417903a586060b523c573f2eb3bff3b735eb4d67
Tombatron <tommy.hanks@gmail.com>
2020-07-11 07:15:02 -0400

    Migrated a test that verifies schema printability.

---
+++ tests/NRediSearch.Test/SchemaTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace NRediSearch.Test
+{
+    public class SchemaTest
+    {
+        [Fact]
+        public void PrintSchemaTest()
+        {
+            var sc = new Schema();
+
+            sc.AddTextField("title", 5.0);
+            sc.AddSortableTextField("plot", 1.0);
+            sc.AddSortableTagField("genre", ",");
+            sc.AddSortableNumericField("release_year");
+            sc.AddSortableNumericField("rating");
+            sc.AddSortableNumericField("votes");
+
+            var schemaPrint = sc.ToString();
+
+            Assert.StartsWith("Schema{fields=[TextField{name='title'", schemaPrint);
+            Assert.Contains("{name='release_year', type=Numeric, sortable=true, noindex=false}", schemaPrint);
+        }
+    }
+}
