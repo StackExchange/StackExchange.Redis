90f4871620a863f7f13ac934040dc8f89ad68655
Tim Lovell-Smith <tilovell@microsoft.com>
2021-06-05 00:32:36 -0700

    Bugfix the bugfix for #1719. Certain scenarios, and TieBreaker in particular, rely on the old send-out-of-order (compared to queueing) behavior of PhysicalBridge, but these are all interna messages, and the out-of-order bugs that impact users are not, so we can filter by those.

--- src/StackExchange.Redis/PhysicalBridge.cs
+++ src/StackExchange.Redis/PhysicalBridge.cs
@@ -716,10 +716,11 @@ namespace StackExchange.Redis
             Trace("Writing: " + message);
             message.SetEnqueued(physical); // this also records the read/write stats at this point
 
-            // AVOID REORDERING MESSAGES
+            // AVOID REORDERING USER MESSAGES
             // Prefer to add it to the backlog if this thread can see that there might already be a message backlog.
             // We do this before attempting to take the writelock, because we won't actually write, we'll just let the backlog get processed in due course
-            if (PushToBacklog(message, onlyIfExists: true))
+            // We currently make an exception for InternalCall because Handshaking relies on being able to 'jump the queue'
+            if (!message.IsInternalCall && PushToBacklog(message, onlyIfExists: true))
             {
                 return WriteResult.Success; // queued counts as success
             }
@@ -964,10 +965,11 @@ namespace StackExchange.Redis
             Trace("Writing: " + message);
             message.SetEnqueued(physical); // this also records the read/write stats at this point
 
-            // AVOID REORDERING MESSAGES
+            // AVOID REORDERING USER MESSAGES
             // Prefer to add it to the backlog if this thread can see that there might already be a message backlog.
             // We do this before attempting to take the writelock, because we won't actually write, we'll just let the backlog get processed in due course
-            if (PushToBacklog(message, onlyIfExists: true))
+            // We currently make an exception for InternalCall because Handshaking relies on being able to 'jump the queue'
+            if (!message.IsInternalCall && PushToBacklog(message, onlyIfExists: true))
             {
                 return new ValueTask<WriteResult>(WriteResult.Success); // queued counts as success
             }
