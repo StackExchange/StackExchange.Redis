bfdfb3b9792a9ee9bba3f306694c5087146abf8f
Deepak Verma <deepakv@microsoft.com>
2021-07-28 22:07:54 -0700

    fixing another diff that should have not been there

--- src/StackExchange.Redis/Enums/CommandFlags.cs
+++ src/StackExchange.Redis/Enums/CommandFlags.cs
@@ -3,11 +3,10 @@ using System.ComponentModel;
 
 namespace StackExchange.Redis
 {
-    /// <summary>
+  /// <summary>
     /// Behaviour markers associated with a given command
     /// </summary>
     [Flags]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1069:Enums values should not be duplicated", Justification = "Compatibility")]
     public enum CommandFlags
     {
         /// <summary>
