1bc971dd6659ac22d7f906f48ab7e9842568e1fe
Nick Craver <nrcraver@gmail.com>
2022-01-24 19:37:21 -0500

    Primary/Replica tests

--- tests/StackExchange.Redis.Tests/PubSubMultiserver.cs
+++ tests/StackExchange.Redis.Tests/PubSubMultiserver.cs
@@ -27,7 +27,7 @@ namespace StackExchange.Redis.Tests
         }
 
         [Fact]
-        public async Task SubscriptionNodeReconnecting()
+        public async Task ClusterNodeSubscriptionFailover()
         {
             Log("Connecting...");
             using var muxer = Create(allowAdmin: true) as ConnectionMultiplexer;
@@ -92,7 +92,95 @@ namespace StackExchange.Redis.Tests
             ClearAmbientFailures();
         }
 
-        // TODO: Primary/Replica failover
-        // TODO: Subscribe failover, but with CommandFlags
+        [Theory]
+        [InlineData(CommandFlags.PreferMaster, true)]
+        [InlineData(CommandFlags.PreferReplica, true)]
+        [InlineData(CommandFlags.DemandMaster, false)]
+        [InlineData(CommandFlags.DemandReplica, false)]
+        public async Task PrimaryReplicaSubscriptionFailover(CommandFlags flags, bool expectSuccess)
+        {
+            var config = TestConfig.Current.MasterServerAndPort + "," + TestConfig.Current.ReplicaServerAndPort;
+            Log("Connecting...");
+            using var muxer = Create(configuration: config, allowAdmin: true) as ConnectionMultiplexer;
+            var sub = muxer.GetSubscriber();
+            var channel = (RedisChannel)Me();
+
+            var count = 0;
+            Log("Subscribing...");
+            await sub.SubscribeAsync(channel, (channel, val) =>
+            {
+                Interlocked.Increment(ref count);
+                Log("Message: " + val);
+            }, flags);
+            Assert.True(sub.IsConnected(channel));
+
+            Log("Publishing (1)...");
+            Assert.Equal(0, count);
+            var publishedTo = await sub.PublishAsync(channel, "message1");
+            // Client -> Redis -> Client -> handler takes just a moment
+            await UntilCondition(TimeSpan.FromSeconds(2), () => Volatile.Read(ref count) == 1);
+            Assert.Equal(1, count);
+            Log($"  Published (1) to {publishedTo} subscriber(s).");
+
+            var endpoint = sub.SubscribedEndpoint(channel);
+            var subscribedServer = muxer.GetServer(endpoint);
+            var subscribedServerEndpoint = muxer.GetServerEndPoint(endpoint);
+
+            Assert.True(subscribedServer.IsConnected, "subscribedServer.IsConnected");
+            Assert.True(subscribedServerEndpoint.IsConnected, "subscribedServerEndpoint.IsConnected");
+            Assert.True(subscribedServerEndpoint.IsSubscriberConnected, "subscribedServerEndpoint.IsSubscriberConnected");
+
+            Assert.True(muxer.TryGetSubscription(channel, out var subscription));
+            var initialServer = subscription.GetCurrentServer();
+            Assert.NotNull(initialServer);
+            Assert.True(initialServer.IsConnected);
+            Log($"Connected to: " + initialServer);
+
+            muxer.AllowConnect = false;
+            subscribedServerEndpoint.SimulateConnectionFailure(SimulatedFailureType.AllSubscription);
+
+            Assert.True(subscribedServerEndpoint.IsConnected, "subscribedServerEndpoint.IsConnected");
+            Assert.False(subscribedServerEndpoint.IsSubscriberConnected, "subscribedServerEndpoint.IsSubscriberConnected");
+
+            if (expectSuccess)
+            {
+                await UntilCondition(TimeSpan.FromSeconds(5), () => subscription.IsConnected);
+                Assert.True(subscription.IsConnected);
+
+                var newServer = subscription.GetCurrentServer();
+                Assert.NotNull(newServer);
+                Assert.NotEqual(newServer, initialServer);
+                Log($"Now connected to: " + newServer);
+            }
+            else
+            {
+                // This subscription shouldn't be able to reconnect by flags (demanding an unavailable server)
+                await UntilCondition(TimeSpan.FromSeconds(2), () => subscription.IsConnected);
+                Assert.False(subscription.IsConnected);
+                Log("Unable to reconnect (as expected)");
+
+                // Allow connecting back to the original
+                muxer.AllowConnect = true;
+                await UntilCondition(TimeSpan.FromSeconds(2), () => subscription.IsConnected);
+                Assert.True(subscription.IsConnected);
+
+                var newServer = subscription.GetCurrentServer();
+                Assert.NotNull(newServer);
+                Assert.Equal(newServer, initialServer);
+                Log($"Now connected to: " + newServer);
+            }
+
+
+            count = 0;
+            Log("Publishing (2)...");
+            Assert.Equal(0, count);
+            publishedTo = await sub.PublishAsync(channel, "message2");
+            // Client -> Redis -> Client -> handler takes just a moment
+            await UntilCondition(TimeSpan.FromSeconds(2), () => Volatile.Read(ref count) == 1);
+            Assert.Equal(1, count);
+            Log($"  Published (2) to {publishedTo} subscriber(s).");
+
+            ClearAmbientFailures();
+        }
     }
 }
