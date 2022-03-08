c092a7e2d5decc042f6437ff3191cd10334ee493
Nick Craver <nrcraver@gmail.com>
2022-03-08 09:43:26 -0500

    Config misses
    This should fix the crazy build timeouts on GHA

--- tests/RedisConfigs/start-failover.cmd
+++ tests/RedisConfigs/start-failover.cmd
@@ -2,7 +2,7 @@
 echo Starting Failover:
 pushd %~dp0\Failover
 echo   Master: 6382
-@start "Redis (Failover Master): 6382" /min ..\3.0.503\redis-server.exe master-6382.conf
+@start "Redis (Failover Master): 6382" /min ..\3.0.503\redis-server.exe primary-6382.conf
 echo   Replica: 6383
 @start "Redis (Failover Replica): 6383" /min ..\3.0.503\redis-server.exe replica-6383.conf
 popd
