cb97f93d3d9062d18dc9839ef0ae591caffb8a7b
Tommy Hanks <tommy.hanks@gmail.com>
2022-04-03 08:45:06 -0400

    Added in a sync and async test case here.

---
+++ tests/StackExchange.Redis.Tests/LolWutTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace StackExchange.Redis.Tests
+{
+    public class LolWutTests : TestBase
+    {
+        public LolWutTests(ITestOutputHelper output, SharedConnectionFixture? fixture = null) : base(output, fixture)
+        {
+        }
+
+        [Fact]
+        public void CanGetRedisVersionWithArt()
+        {
+            using var connection = Create();
+            var db = connection.GetDatabase();
+
+            var result = db.LolWut();
+
+            Assert.Contains("Redis ver", result.ToString());
+        }
+
+        [Fact]
+        public async Task CanGetRedisVersionWithArtAsync()
+        {
+            using var connection = Create();
+            var db = connection.GetDatabase();
+
+            var result = await db.LolWutAsync();
+
+            Assert.Contains("Redis ver", result.ToString());
+        }
+    }
+}
+
+
+
