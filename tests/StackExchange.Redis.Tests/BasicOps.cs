380c0470a1e632da320fef24428d98bb36678092
Avital-Fine <avital.fine@redis.com>
2022-04-04 16:17:28 +0300

    more tests

--- tests/StackExchange.Redis.Tests/BasicOps.cs
+++ tests/StackExchange.Redis.Tests/BasicOps.cs
@@ -448,6 +448,46 @@ namespace StackExchange.Redis.Tests
         }
 
         [Fact]
+        public async Task CopyCrossDB()
+        {
+            using (var muxer = Create())
+            {
+                var db = muxer.GetDatabase();
+                var dbDestId = TestConfig.GetDedicatedDB(muxer);
+                var dbDest = muxer.GetDatabase(dbDestId);
+
+                var src = Me();
+                var dest = Me() + "2";
+                dbDest.KeyDelete(dest);
+
+                _ = db.StringSetAsync(src, "Heyyyyy");
+                var ke1 = db.KeyCopyAsync(src, dest, dbDestId).ForAwait();
+                var ku1 = dbDest.StringGet(dest);
+                Assert.True(await ke1);
+                Assert.True(ku1.Equals("Heyyyyy"));
+            }
+        }
+
+        [Fact]
+        public async Task CopyWithReplace()
+        {
+            using (var muxer = Create())
+            {
+                var db = muxer.GetDatabase();
+                var src = Me();
+                var dest = Me() + "2";
+                _ = db.StringSetAsync(src, "foo1");
+                _ = db.StringSetAsync(dest, "foo2");
+                var ke1 = db.KeyCopyAsync(src, dest).ForAwait();
+                var ke2 = db.KeyCopyAsync(src, dest, replace: true).ForAwait();
+                var ku1 = db.StringGet(dest);
+                Assert.False(await ke1); // Should fail when not using replace and destination key exists
+                Assert.True(await ke2);
+                Assert.True(ku1.Equals("foo1"));
+            }
+        }
+
+        [Fact]
         public async Task Delete()
         {
             using (var muxer = Create())
