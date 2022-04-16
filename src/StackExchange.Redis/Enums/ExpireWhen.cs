c17f6440eb8d3fdbe94149e3dc86b2c575d5433b
Nick Craver <nrcraver@gmail.com>
2022-04-15 21:25:26 -0400

    C# update

--- src/StackExchange.Redis/Enums/ExpireWhen.cs
+++ src/StackExchange.Redis/Enums/ExpireWhen.cs
@@ -1,43 +1,42 @@
 ﻿using System;
 
-namespace StackExchange.Redis
+namespace StackExchange.Redis;
+
+/// <summary>
+/// Specifies when to set the expiry for a key.
+/// </summary>
+public enum ExpireWhen
 {
     /// <summary>
-    /// Specifies when to set the expiry for a key.
+    /// Set expiry whether or not there is an existing expiry.
     /// </summary>
-    public enum ExpireWhen
-    {
-        /// <summary>
-        /// Set expiry whether or not there is an existing expiry.
-        /// </summary>
-        Always,
-        /// <summary>
-        /// Set expiry only when the new expiry is greater than current one.
-        /// </summary>
-        GreaterThanCurrentExpiry,
-        /// <summary>
-        /// Set expiry only when the key has an existing expiry.
-        /// </summary>
-        HasExpiry,
-        /// <summary>
-        /// Set expiry only when the key has no expiry.
-        /// </summary>
-        HasNoExpiry,
-        /// <summary>
-        /// Set expiry only when the new expiry is less than current one.
-        /// </summary>
-        LessThanCurrentExpiry,
-    }
+    Always,
+    /// <summary>
+    /// Set expiry only when the new expiry is greater than current one.
+    /// </summary>
+    GreaterThanCurrentExpiry,
+    /// <summary>
+    /// Set expiry only when the key has an existing expiry.
+    /// </summary>
+    HasExpiry,
+    /// <summary>
+    /// Set expiry only when the key has no expiry.
+    /// </summary>
+    HasNoExpiry,
+    /// <summary>
+    /// Set expiry only when the new expiry is less than current one
+    /// </summary>
+    LessThanCurrentExpiry,
+}
 
-    internal static class ExpiryOptionExtensions
+internal static class ExpiryOptionExtensions
+{
+    public static RedisValue ToLiteral(this ExpireWhen op) => op switch
     {
-        public static RedisValue ToLiteral(this ExpireWhen op) => op switch
-        {
-            ExpireWhen.HasNoExpiry => RedisLiterals.NX,
-            ExpireWhen.HasExpiry => RedisLiterals.XX,
-            ExpireWhen.GreaterThanCurrentExpiry => RedisLiterals.GT,
-            ExpireWhen.LessThanCurrentExpiry => RedisLiterals.LT,
-            _ => throw new ArgumentOutOfRangeException(nameof(op))
-        };
-    }
+        ExpireWhen.HasNoExpiry => RedisLiterals.NX,
+        ExpireWhen.HasExpiry => RedisLiterals.XX,
+        ExpireWhen.GreaterThanCurrentExpiry => RedisLiterals.GT,
+        ExpireWhen.LessThanCurrentExpiry => RedisLiterals.LT,
+        _ => throw new ArgumentOutOfRangeException(nameof(op))
+    };
 }
