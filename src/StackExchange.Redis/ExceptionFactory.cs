b2c538d7c60d23e7feeafc2ff22dde321a0cabd9
Nick Craver <nrcraver@gmail.com>
2022-04-18 22:27:10 -0400

    Restrict message more

--- src/StackExchange.Redis/ExceptionFactory.cs
+++ src/StackExchange.Redis/ExceptionFactory.cs
@@ -124,7 +124,7 @@ namespace StackExchange.Redis
                 // This can happen in cloud environments often, where user disables abort and has the wrong config
                 initialMessage = $"Connection to Redis never succeeded (attempts: {attempts} - check your config), unable to service operation: ";
             }
-            else if (message is not null && message.IsPrimaryOnly())
+            else if (message is not null && message.IsPrimaryOnly() && multiplexer.IsConnected)
             {
                 // If we know it's a primary-only command, indicate that in the error message
                 initialMessage = "No connection (requires writable - not eligible for replica) is active/available to service this operation: ";
