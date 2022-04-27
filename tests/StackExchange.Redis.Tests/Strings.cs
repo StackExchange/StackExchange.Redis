823d3ec6c3c0ef6574ad66701e90ed6e0c07ee50
Avital-Fine <avital.fine@redis.com>
2022-04-27 21:25:46 +0300

    move tests to OverloadCompat

--- tests/StackExchange.Redis.Tests/Strings.cs
+++ tests/StackExchange.Redis.Tests/Strings.cs
@@ -544,60 +544,6 @@ public class Strings : TestBase
     }
 
     [Fact]
-    public async Task BitCountOverload()
-    {
-        using var conn = Create(require: RedisFeatures.v2_6_0);
-
-        var db = conn.GetDatabase();
-        var key = Me();
-        db.KeyDelete(key, flags: CommandFlags.FireAndForget);
-        db.StringSet(key, "foobar", flags: CommandFlags.FireAndForget);
-
-        var r1 = db.StringBitCount(key);
-        var r2 = db.StringBitCount(key, start: 0, end: 0);
-        var r3 = db.StringBitCount(key, start: 1);
-        var r4 = db.StringBitCount(key, end: 1);
-
-        Assert.Equal(26, r1);
-        Assert.Equal(4, r2);
-        Assert.Equal(22, r3);
-        Assert.Equal(10, r4);
-
-        var flags = CommandFlags.None;
-        r1 = db.StringBitCount(key, flags: flags);
-        r2 = db.StringBitCount(key, start: 0, end: 0, flags: flags);
-        r3 = db.StringBitCount(key, start: 1, flags: flags);
-        r4 = db.StringBitCount(key, end: 1, flags: flags);
-
-        Assert.Equal(26, r1);
-        Assert.Equal(4, r2);
-        Assert.Equal(22, r3);
-        Assert.Equal(10, r4);
-
-        // Async
-
-        r1 = await db.StringBitCountAsync(key);
-        r2 = await db.StringBitCountAsync(key, start: 0, end: 0);
-        r3 = await db.StringBitCountAsync(key, start: 1);
-        r4 = await db.StringBitCountAsync(key, end: 1);
-
-        Assert.Equal(26, r1);
-        Assert.Equal(4, r2);
-        Assert.Equal(22, r3);
-        Assert.Equal(10, r4);
-
-        r1 = await db.StringBitCountAsync(key, flags: flags);
-        r2 = await db.StringBitCountAsync(key, start: 0, end: 0, flags: flags);
-        r3 = await db.StringBitCountAsync(key, start: 1, flags: flags);
-        r4 = await db.StringBitCountAsync(key, end: 1, flags: flags);
-
-        Assert.Equal(26, r1);
-        Assert.Equal(4, r2);
-        Assert.Equal(22, r3);
-        Assert.Equal(10, r4);
-    }
-
-    [Fact]
     public async Task BitOp()
     {
         using var conn = Create(require: RedisFeatures.v2_6_0);
@@ -676,60 +622,6 @@ public class Strings : TestBase
 
         Assert.Equal(9, await r1);
         Assert.Equal(1, await r2);
-    }
-
-    [Fact]
-    public async Task BitPositionOverload()
-    {
-        using var conn = Create(require: RedisFeatures.v2_6_0);
-
-        var db = conn.GetDatabase();
-        var key = Me();
-        db.KeyDelete(key, flags: CommandFlags.FireAndForget);
-        db.StringSet(key, "foo", flags: CommandFlags.FireAndForget);
-
-        var r1 = db.StringBitPosition(key, true);
-        var r2 = db.StringBitPosition(key, true, start: 1, end: 3);
-        var r3 = db.StringBitPosition(key, true, start: 1);
-        var r4 = db.StringBitPosition(key, true, end: 3);
-
-        Assert.Equal(1, r1);
-        Assert.Equal(9, r2);
-        Assert.Equal(9, r3);
-        Assert.Equal(1, r4);
-
-        var flags = CommandFlags.None;
-        r1 = db.StringBitPosition(key, true, flags: flags);
-        r2 = db.StringBitPosition(key, true, start: 1, end: 3, flags: flags);
-        r3 = db.StringBitPosition(key, true, start: 1, flags: flags);
-        r4 = db.StringBitPosition(key, true, end: 3, flags: flags);
-
-        Assert.Equal(1, r1);
-        Assert.Equal(9, r2);
-        Assert.Equal(9, r3);
-        Assert.Equal(1, r4);
-
-        // Async
-
-        r1 = await db.StringBitPositionAsync(key, true);
-        r2 = await db.StringBitPositionAsync(key, true, start: 1, end: 3);
-        r3 = await db.StringBitPositionAsync(key, true, start: 1);
-        r4 = await db.StringBitPositionAsync(key, true, end: 3);
-
-        Assert.Equal(1, r1);
-        Assert.Equal(9, r2);
-        Assert.Equal(9, r3);
-        Assert.Equal(1, r4);
-
-        r1 = await db.StringBitPositionAsync(key, true, flags: flags);
-        r2 = await db.StringBitPositionAsync(key, true, start: 1, end: 3, flags: flags);
-        r3 = await db.StringBitPositionAsync(key, true, start: 1, flags: flags);
-        r4 = await db.StringBitPositionAsync(key, true, end: 3, flags: flags);
-
-        Assert.Equal(1, r1);
-        Assert.Equal(9, r2);
-        Assert.Equal(9, r3);
-        Assert.Equal(1, r4);
     }
 
     [Fact]
