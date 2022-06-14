205713a30a42a8c1c8ed783235311f07654c8bd3
Nick Craver <nrcraver@gmail.com>
2022-06-14 09:46:34 -0400

    Code tweak and release notes

--- src/StackExchange.Redis/Enums/SortedSetWhen.cs
+++ src/StackExchange.Redis/Enums/SortedSetWhen.cs
@@ -32,12 +32,12 @@ namespace StackExchange.Redis
 
     internal static class SortedSetWhenExtensions
     {
-        internal static UInt32 CountBits(this SortedSetWhen when)
+        internal static uint CountBits(this SortedSetWhen when)
         {
-            UInt32 v = (UInt32)when;
-            v = v - ((v >> 1) & 0x55555555); // reuse input as temporary
+            uint v = (uint)when;
+            v -= ((v >> 1) & 0x55555555); // reuse input as temporary
             v = (v & 0x33333333) + ((v >> 2) & 0x33333333); // temp
-            UInt32 c = ((v + (v >> 4) & 0xF0F0F0F) * 0x1010101) >> 24; // count
+            uint c = ((v + (v >> 4) & 0xF0F0F0F) * 0x1010101) >> 24; // count
             return c;
         }
 
@@ -50,5 +50,3 @@ namespace StackExchange.Redis
         };
     }
 }
-
-
