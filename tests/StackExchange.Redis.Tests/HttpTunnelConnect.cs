ba8b70ec82f31302ba9f529965ca011685566ea2
Marc Gravell <marc.gravell@gmail.com>
2022-10-18 13:19:45 +0100

    add latency/bandwidth tests for http-tunnel

--- tests/StackExchange.Redis.Tests/HttpTunnelConnect.cs
+++ tests/StackExchange.Redis.Tests/HttpTunnelConnect.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace StackExchange.Redis.Tests
 {
     public class HttpTunnelConnect
     {
+        private ITestOutputHelper Log { get; }
+        public HttpTunnelConnect(ITestOutputHelper log) => Log = log;
         [Theory]
         [InlineData("")]
         [InlineData(",gateway=http:127.0.0.1:8080")]
@@ -23,7 +27,37 @@ namespace StackExchange.Redis.Tests
                 Assert.NotNull(config.BeforeAuthenticate);
             }
             await using var conn = await ConnectionMultiplexer.ConnectAsync(config);
-            await conn.GetDatabase().PingAsync();
+            var db = conn.GetDatabase();
+            await db.PingAsync();
+            RedisKey key = "HttpTunnel";
+            await db.KeyDeleteAsync(key);
+
+            // latency test
+            var watch = Stopwatch.StartNew();
+            const int LATENCY_LOOP = 25, BANDWIDTH_LOOP = 10;
+            for (int i = 0; i < LATENCY_LOOP; i++)
+            {
+                await db.StringIncrementAsync(key);
+            }
+            watch.Stop();
+            int count = (int)await db.StringGetAsync(key);
+            Log.WriteLine($"{LATENCY_LOOP}xINCR: {watch.ElapsedMilliseconds}ms");
+            Assert.Equal(LATENCY_LOOP, count);
+
+            // bandwidth test
+            var chunk = new byte[4096];
+            var rand = new Random(1234);
+            for (int i = 0; i < BANDWIDTH_LOOP; i++)
+            {
+                rand.NextBytes(chunk);
+                watch = Stopwatch.StartNew();
+                await db.StringSetAsync(key, chunk);
+                using var fetch = await db.StringGetLeaseAsync(key);
+                watch.Stop();
+                Assert.NotNull(fetch);
+                Log.WriteLine($"SET+GET {chunk.Length} bytes: {watch.ElapsedMilliseconds}ms");
+                Assert.True(fetch.Span.SequenceEqual(chunk));
+            }
         }
     }
 }
