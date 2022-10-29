cccde5a3298f9310627f859270bee6ca880e9f63
Nick Craver <nrcraver@gmail.com>
2022-10-29 09:04:25 -0400

    Simplify to KeyPrefixedSubscriber only

--- StackExchange.Redis/StackExchange/Redis/KeyspaceIsolation/KeyPrefixedSubscriber.cs
+++ StackExchange.Redis/StackExchange/Redis/KeyspaceIsolation/KeyPrefixedSubscriber.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace StackExchange.Redis.KeyspaceIsolation
 {
-    internal class SubscriberWrapper : ISubscriber
+    internal class KeyPrefixedSubscriber : ISubscriber
     {
         public ISubscriber Inner { get; }
 
