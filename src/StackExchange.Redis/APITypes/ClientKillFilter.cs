70302e7d28a179bbf78b8120e146c249ad708c52
Nick Craver <nickcraver@microsoft.com>
2024-07-02 11:13:29 -0400

    Fix up formatting and docs

--- src/StackExchange.Redis/APITypes/ClientKillFilter.cs
+++ src/StackExchange.Redis/APITypes/ClientKillFilter.cs
@@ -1,143 +1,120 @@
-using System;
-using System.Net;
+﻿using System.Net;
 
 namespace StackExchange.Redis;
 
 /// <summary>
-/// 
+/// Filter determining which Redis clients to kill.
 /// </summary>
+/// <seealso href="https://redis.io/docs/latest/commands/client-kill/"/>
 public class ClientKillFilter
 {
     /// <summary>
-    /// Filter arguments for ClientKill
+    /// Filter arguments builder for `CLIENT KILL`.
     /// </summary>
     public ClientKillFilter() { }
 
     /// <summary>
     /// The ID of the client to kill.
     /// </summary>
-    public long? Id
-    {
-        get; private set;
-    }
+    public long? Id { get; private set; }
 
     /// <summary>
     /// The type of client.
     /// </summary>
-    public ClientType? ClientType
-    {
-        get; private set;
-    }
+    public ClientType? ClientType { get; private set; }
 
     /// <summary>
-    /// The authenticated ACL username, 
+    /// The authenticated ACL username.
     /// </summary>
-    /// <value></value>
-    public string? Username
-    {
-        get; private set;
-    }
+    public string? Username { get; private set; }
 
     /// <summary>
     /// The endpoint to kill.
     /// </summary>
-    public EndPoint? Endpoint
-    {
-        get; private set;
-    }
+    public EndPoint? Endpoint { get; private set; }
 
     /// <summary>
     /// The server endpoint to kill.
     /// </summary>
-    public EndPoint? ServerEndpoint
-    {
-        get; private set;
-    }
+    public EndPoint? ServerEndpoint { get; private set; }
 
     /// <summary>
     /// Whether to skip the current connection.
     /// </summary>
-    public bool? SkipMe
-    {
-        get; private set;
-    }
+    public bool? SkipMe { get; private set; }
 
     /// <summary>
-    /// Age of connection in seconds
+    /// Age of connection in seconds.
     /// </summary>
-    /// <value></value>
-    public long? MaxAgeInSeconds
-    {
-        get; private set;
-    }
+    public long? MaxAgeInSeconds { get; private set; }
 
     /// <summary>
-    /// Set id filter
+    /// Sets client id filter.
     /// </summary>
-    /// <param name="id">Id of the client</param>
-    /// <returns></returns>
+    /// <param name="id">Id of the client to kill.</param>
     public ClientKillFilter WithId(long id)
     {
-        Id = id; return this;
+        Id = id;
+        return this;
     }
 
     /// <summary>
-    /// Set client type
+    /// Sets client type filter.
     /// </summary>
-    /// <param name="clientType">The type of the client</param>
-    /// <returns></returns>
+    /// <param name="clientType">The type of the client.</param>
     public ClientKillFilter WithClientType(ClientType clientType)
     {
-        ClientType = clientType; return this;
+        ClientType = clientType;
+        return this;
     }
 
     /// <summary>
-    /// Set username
+    /// Sets the username filter.
     /// </summary>
-    /// <param name="username">Authenticated ACL username</param>
-    /// <returns></returns>
+    /// <param name="username">Authenticated ACL username.</param>
     public ClientKillFilter WithUsername(string username)
     {
-        Username = username; return this;
+        Username = username;
+        return this;
     }
 
     /// <summary>
-    /// Set endpoint
+    /// Set the endpoint filter.
     /// </summary>
     /// <param name="endpoint">The endpoint to kill.</param>
-    /// <returns></returns>
     public ClientKillFilter WithEndpoint(EndPoint endpoint)
     {
-        Endpoint = endpoint; return this;
+        Endpoint = endpoint;
+        return this;
     }
 
     /// <summary>
-    /// Set server endpoint
+    /// Set the server endpoint filter.
     /// </summary>
     /// <param name="serverEndpoint">The server endpoint to kill.</param>
-    /// <returns></returns>
     public ClientKillFilter WithServerEndpoint(EndPoint serverEndpoint)
     {
-        ServerEndpoint = serverEndpoint; return this;
+        ServerEndpoint = serverEndpoint;
+        return this;
     }
 
     /// <summary>
-    /// Set skipMe
+    /// Set the skipMe filter (whether to skip the current connection).
     /// </summary>
     /// <param name="skipMe">Whether to skip the current connection.</param>
-    /// <returns></returns>
     public ClientKillFilter WithSkipMe(bool skipMe)
     {
-        SkipMe = skipMe; return this;
+        SkipMe = skipMe;
+        return this;
     }
 
     /// <summary>
-    /// Set MaxAgeInSeconds
+    /// Set the MaxAgeInSeconds filter.
     /// </summary>
     /// <param name="maxAgeInSeconds">Age of connection in seconds</param>
-    /// <returns></returns>
     public ClientKillFilter WithMaxAgeInSeconds(long maxAgeInSeconds)
     {
-        MaxAgeInSeconds = maxAgeInSeconds; return this;
+        MaxAgeInSeconds = maxAgeInSeconds;
+        return this;
     }
 }
