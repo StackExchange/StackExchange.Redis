8764546dd04b9960478554484aac3da7ca40f399
Nick Craver <nrcraver@gmail.com>
2024-08-03 11:09:39 -0400

    Cleanup

--- src/StackExchange.Redis/RedisDatabase.cs
+++ src/StackExchange.Redis/RedisDatabase.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Buffers;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Pipelines.Sockets.Unofficial.Arenas;
