44d6213c16462ae0072f74f3e1fef0a4e1853be4
Marc Gravell <marc.gravell@gmail.com>
2024-11-05 18:45:23 +0000

    start

---
+++ src/resp-cli/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.CommandLine;
+using System.Reflection;
+
+// https://learn.microsoft.com/en-us/dotnet/standard/commandline/get-started-tutorial
+RootCommand rootCommand = new RootCommand(
+    description: "Connects to a RESP server to issue ad-hoc commands.");
+rootCommand.AddOption(new Option<string>(name: "host", description: "The RESP endpoint to connect to.").AddAlias("h")
+rootCommand.AddArgument(new Argument<string>()
+var method = typeof(RespClient).GetMethod(nameof(RespClient.RunClient))!;
+rootCommand.Add(new Option
+{
+   
+
+})
+//    .ConfigureFromMethod(method);
+//rootCommand.Children["--input"].AddAlias("-i");
+//rootCommand.Children["--output"].AddAlias("-o");
+return await rootCommand.InvokeAsync(args);
+
+static class RespClient
+{
+    public static void RunClient()
+    {
+        string? line;
+        while ((line = ReadLine()) is not null)
+        {
+            Console.WriteLine("< " + line);
+        }
+
+        static string? ReadLine()
+        {
+            Console.Write("> ");
+            return Console.ReadLine();
+        }
+    }
+
+}
