2f9f392714b8254667eec01bbf926c24f9ab8e76
Marc Gravell <marc.gravell@gmail.com>
2024-11-05 19:47:33 +0000

    first working

--- src/resp-cli/Program.cs
+++ src/resp-cli/Program.cs
@@ -1,38 +1,107 @@
-﻿
-using System.CommandLine;
-using System.Reflection;
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.Globalization;
+using System.Net;
+using StackExchange.Redis;
 
-// https://learn.microsoft.com/en-us/dotnet/standard/commandline/get-started-tutorial
-RootCommand rootCommand = new RootCommand(
-    description: "Connects to a RESP server to issue ad-hoc commands.");
-rootCommand.AddOption(new Option<string>(name: "host", description: "The RESP endpoint to connect to.").AddAlias("h")
-rootCommand.AddArgument(new Argument<string>()
-var method = typeof(RespClient).GetMethod(nameof(RespClient.RunClient))!;
-rootCommand.Add(new Option
+Option<string> hostOption = new(
+    aliases: new[] { "--host", "-h" },
+    description: "The host of the RESP server.");
+hostOption.SetDefaultValue("127.0.0.1");
+
+Option<int> portOption = new(
+    aliases: new[] { "--port", "-p" },
+    description: "The port of the RESP server.");
+portOption.SetDefaultValue(6379);
+
+RootCommand rootCommand = new(description: "Connects to a RESP server to issue ad-hoc commands.")
 {
-   
+    hostOption,
+    portOption,
+};
 
-})
-//    .ConfigureFromMethod(method);
-//rootCommand.Children["--input"].AddAlias("-i");
-//rootCommand.Children["--output"].AddAlias("-o");
+rootCommand.SetHandler(
+    (string host, int port) => RespClient.RunClient(host, port),
+    hostOption,
+    portOption);
 return await rootCommand.InvokeAsync(args);
 
-static class RespClient
+file static class RespClient
 {
-    public static void RunClient()
+    public static async Task RunClient(string host, int port)
     {
-        string? line;
-        while ((line = ReadLine()) is not null)
+        EndPoint ep;
+        if (IPAddress.TryParse(host, out var ipAddress))
         {
-            Console.WriteLine("< " + line);
+            ep = new IPEndPoint(ipAddress, port);
+        }
+        else
+        {
+            ep = new DnsEndPoint(host, port);
+        }
+        var config = new ConfigurationOptions
+        {
+            EndPoints = { ep },
+        };
+        Console.WriteLine($"Connecting to {config}...");
+        config.AllowAdmin = true;
+        using var muxer = await ConnectionMultiplexer.ConnectAsync(config);
+
+        var (connected, dbCount) = await VerifyConnectedAsync(muxer);
+        if (!connected) return;
+
+        var db = muxer.GetDatabase();
+        while (true)
+        {
+            var line = ReadLine(db);
+            if (line is null) break;
+            if (string.IsNullOrWhiteSpace(line)) continue;
+            var parts = line.Split(" ");
+
+            object[] args = new string[parts.Length - 1];
+            Array.Copy(parts, 1, args, 0, parts.Length - 1);
+            var result = await db.ExecuteAsync(parts[0], args);
+
+            Console.WriteLine($"< {result}");
         }
 
-        static string? ReadLine()
+        static async Task<(bool Connected, int Databases)> VerifyConnectedAsync(ConnectionMultiplexer muxer)
         {
-            Console.Write("> ");
+            foreach (var server in muxer.GetServers())
+            {
+                // show some details about the first server we connect to
+                if (server.IsConnected)
+                {
+                    foreach (var grp in await server.InfoAsync("server"))
+                    {
+                        Console.WriteLine($"# {grp.Key}");
+                        foreach (var pair in grp)
+                        {
+                            if (pair.Key.EndsWith("version") || pair.Key == "redis_mode")
+                            {
+                                Console.WriteLine($"{pair.Key}:\t{pair.Value}");
+                            }
+                        }
+                    }
+                    int dbCount = -1;
+                    foreach (var token in await server.ConfigGetAsync("databases"))
+                    {
+                        Console.WriteLine($"{token.Key}:\t{token.Value}");
+                        if (int.TryParse(token.Value, NumberStyles.Integer, CultureInfo.InvariantCulture, out var tmp))
+                        {
+                            dbCount = tmp;
+                        }
+                    }
+                    return (true, dbCount);
+                }
+            }
+            return (false, -1);
+        }
+
+        static string? ReadLine(IDatabase db)
+        {
+            Console.Write($"[{db.Database}]> ");
             return Console.ReadLine();
         }
     }
-
 }
