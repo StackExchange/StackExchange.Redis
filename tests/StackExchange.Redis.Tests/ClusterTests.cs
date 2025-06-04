c05fdf65e573bda45b8324fcbc7e76a3bb2798c1
atakavci <a_takavci@yahoo.com>
2025-06-04 12:41:07 +0300

    test hashslot migration with subscription to sharded pubsub

--- tests/StackExchange.Redis.Tests/ClusterTests.cs
+++ tests/StackExchange.Redis.Tests/ClusterTests.cs
@@ -750,7 +750,7 @@ public class ClusterTests : TestBase
     }
 
     [Fact]
-    public async Task TestSubscriberReconnected()
+    public async Task TestShardedPubsubSubscriberAgainstReconnects()
     {
         var channel = RedisChannel.Sharded("testShardChannel");
         using var conn = Create(allowAdmin: true, keepAlive: 1, connectTimeout: 3000, shared: false);
@@ -801,6 +801,118 @@ public class ClusterTests : TestBase
         Assert.Equal(1, conn.GetSubscriptionsCount());
         Assert.Equal(10, received.Count);
         ClearAmbientFailures();
+    }
+
+    [Fact]
+    public async Task TestShardedPubsubSubscriberAgainsHashSlotMigration()
+    {
+        var channel = RedisChannel.Sharded("testShardChannel");
+        using var conn = Create(allowAdmin: true, keepAlive: 1, connectTimeout: 3000, shared: false);
+        Assert.True(conn.IsConnected);
+        var db = conn.GetDatabase();
+        Assert.Equal(0, await db.PublishAsync(channel, "noClientReceivesThis"));
+        await Task.Delay(50); // let the sub settle (this isn't needed on RESP3, note)
+
+        var pubsub = conn.GetSubscriber();
+        List<(RedisChannel, RedisValue)> received = new();
+        var queue = await pubsub.SubscribeAsync(channel);
+        _ = Task.Run(async () =>
+        {
+            // use queue API to have control over order
+            await foreach (var item in queue)
+            {
+                lock (received)
+                {
+                    if (item.Channel.IsSharded && item.Channel == channel) received.Add((item.Channel, item.Message));
+                }
+            }
+        });
+        Assert.Equal(1, conn.GetSubscriptionsCount());
+
+        await Task.Delay(50); // let the sub settle (this isn't needed on RESP3, note)
+        await db.PingAsync();
+
+        for (int i = 0; i < 5; i++)
+        {
+            // check we get a hit
+            Assert.Equal(1, await db.PublishAsync(channel, i.ToString()));
+        }
+        await Task.Delay(50); // let the sub settle (this isn't needed on RESP3, note)
+
+        // lets migrate the slot for "testShardChannel" to another node
+        DoHashSlotMigration();
+
+        await Task.Delay(4000);
+        for (int i = 0; i < 5; i++)
+        {
+            // check we get a hit
+            Assert.Equal(1, await db.PublishAsync(channel, i.ToString()));
+        }
+        await Task.Delay(50); // let the sub settle (this isn't needed on RESP3, note)
+
+        Assert.Equal(1, conn.GetSubscriptionsCount());
+        Assert.Equal(10, received.Count);
+        RollbackHashSlotMigration();
+        ClearAmbientFailures();
+    }
+
+    private void DoHashSlotMigration()
+    {
+        MigrateSlotForTestShardChannel(false);
+    }
+    private void RollbackHashSlotMigration()
+    {
+        MigrateSlotForTestShardChannel(true);
+    }
+
+    private void MigrateSlotForTestShardChannel(bool rollback)
+    {
+        int hashSlotForTestShardChannel = 7177;
+        using var conn = Create(allowAdmin: true, keepAlive: 1, connectTimeout: 5000, shared: false);
+        var servers = conn.GetServers();
+        IServer? serverWithPort7000 = null;
+        IServer? serverWithPort7001 = null;
+
+        string nodeIdForPort7000 = "780813af558af81518e58e495d63b6e248e80adf";
+        string nodeIdForPort7001 = "ea828c6074663c8bd4e705d3e3024d9d1721ef3b";
+        foreach (var server in servers)
+        {
+            string id = server.Execute("CLUSTER", "MYID").ToString();
+            if (id == nodeIdForPort7000)
+            {
+                serverWithPort7000 = server;
+            }
+            if (id == nodeIdForPort7001)
+            {
+                serverWithPort7001 = server;
+            }
+        }
+
+        IServer fromServer, toServer;
+        string fromNode, toNode;
+        int toPort;
+        if (rollback)
+        {
+            fromServer = serverWithPort7000!;
+            fromNode = nodeIdForPort7000;
+            toServer = serverWithPort7001!;
+            toNode = nodeIdForPort7001;
+            toPort = 7001;
+        }
+        else
+        {
+            fromServer = serverWithPort7001!;
+            fromNode = nodeIdForPort7001;
+            toServer = serverWithPort7000!;
+            toNode = nodeIdForPort7000;
+            toPort = 7000;
+        }
+
+        Assert.Equal("OK", toServer.Execute("CLUSTER", "SETSLOT", hashSlotForTestShardChannel, "IMPORTING", fromNode).ToString());
+        Assert.Equal("OK", fromServer.Execute("CLUSTER", "SETSLOT", hashSlotForTestShardChannel, "MIGRATING", toNode).ToString());
+        Assert.Equal("OK", fromServer.Execute("MIGRATE", "127.0.0.1", toPort, "", 0, 5000, "KEYS", "testShardChannel").ToString());
+        Assert.Equal("OK", toServer.Execute("CLUSTER", "SETSLOT", hashSlotForTestShardChannel, "NODE", toNode).ToString());
+        Assert.Equal("OK", fromServer!.Execute("CLUSTER", "SETSLOT", hashSlotForTestShardChannel, "NODE", toNode).ToString());
     }
 
     [Theory]
