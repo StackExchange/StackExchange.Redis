b995ebccc661acd637c6d746fe47ee44b12c1479
Nick Craver <nrcraver@gmail.com>
2025-07-25 10:47:29 -0400

    Tests: Add benchmark suite for easily measuring improvements (#2931)
    * Tests: Add benchmark suite for easily measuring improvements
    
    * - fix mono
    - add double parse
    - add double format
    
    * limit netfx to windows
    
    * tyop
    
    ---------
    
    Co-authored-by: Marc Gravell <marc.gravell@gmail.com>

---
+++ tests/StackExchange.Redis.Benchmarks/SlowConfig.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Jobs;
+
+namespace StackExchange.Redis.Benchmarks
+{
+    internal class SlowConfig : CustomConfig
+    {
+        protected override Job Configure(Job j)
+            => j.WithLaunchCount(1)
+                .WithWarmupCount(1)
+                .WithIterationCount(5);
+    }
+}
---
+++ tests/StackExchange.Redis.Benchmarks/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dotnet run --framework net8.0 -c Release "$@"
