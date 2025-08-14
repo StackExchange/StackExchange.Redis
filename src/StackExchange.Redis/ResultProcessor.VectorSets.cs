c421ccbd93268945ad04a0a4914f68e06db2a568
Marc Gravell <marc.gravell@gmail.com>
2025-08-14 17:17:39 +0100

    literals can be private

--- src/StackExchange.Redis/ResultProcessor.VectorSets.cs
+++ src/StackExchange.Redis/ResultProcessor.VectorSets.cs
@@ -122,15 +122,15 @@ internal abstract partial class ResultProcessor
 #pragma warning disable CS8981, SA1134, SA1300, SA1303, SA1502
         // ReSharper disable InconsistentNaming - to better represent expected literals
         // ReSharper disable IdentifierTypo
-        [FastHash] public static partial class bin { }
-        [FastHash] public static partial class f32 { }
-        [FastHash] public static partial class int8 { }
-        [FastHash] public static partial class size { }
-        [FastHash] public static partial class vset_uid { }
-        [FastHash] public static partial class max_level { }
-        [FastHash] public static partial class quant_type { }
-        [FastHash] public static partial class vector_dim { }
-        [FastHash] public static partial class hnsw_max_node_uid { }
+        [FastHash] private static partial class bin { }
+        [FastHash] private static partial class f32 { }
+        [FastHash] private static partial class int8 { }
+        [FastHash] private static partial class size { }
+        [FastHash] private static partial class vset_uid { }
+        [FastHash] private static partial class max_level { }
+        [FastHash] private static partial class quant_type { }
+        [FastHash] private static partial class vector_dim { }
+        [FastHash] private static partial class hnsw_max_node_uid { }
         // ReSharper restore InconsistentNaming
         // ReSharper restore IdentifierTypo
 #pragma warning restore CS8981, SA1134, SA1300, SA1303, SA1502
