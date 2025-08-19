4d2da7abdc0cbd166bc5670035c51629e1750454
Marc Gravell <marc.gravell@gmail.com>
2025-08-19 15:59:53 +0100

    Implement GetServer(RedisKey, ...) (#2936)
    * - implement GetServer API from #2932, and tests
    - implement memoization on GetServer when asyncState is null
    - fixup NotImplementedException in dummy places
    
    * release notes and shipped
    
    * remove params overload
    
    * remove params overload from muxer impl
    
    * test stability: allow both embstr or raw for string encoding
    
    * remove pragma
    
    * Additional intellisense comments to clarify usage.

--- src/StackExchange.Redis/Interfaces/IServer.cs
+++ src/StackExchange.Redis/Interfaces/IServer.cs
@@ -266,10 +266,13 @@ namespace StackExchange.Redis
         /// <inheritdoc cref="Execute(string, object[])"/>
         Task<RedisResult> ExecuteAsync(string command, params object[] args);
 
+#pragma warning disable RS0026, RS0027 // multiple overloads
         /// <summary>
         /// Execute an arbitrary command against the server; this is primarily intended for
         /// executing modules, but may also be used to provide access to new features that lack
-        /// a direct API.
+        /// a direct API. The command is assumed to be not database-specific. If this is not the case,
+        /// <see cref="Execute(int?, string, ICollection{object}, CommandFlags)"/> should be used to
+        /// specify the database (using <langword>null</langword> to use the configured default database).
         /// </summary>
         /// <param name="command">The command to run.</param>
         /// <param name="args">The arguments to pass for the command.</param>
@@ -280,6 +283,23 @@ namespace StackExchange.Redis
 
         /// <inheritdoc cref="Execute(string, ICollection{object}, CommandFlags)"/>
         Task<RedisResult> ExecuteAsync(string command, ICollection<object> args, CommandFlags flags = CommandFlags.None);
+#pragma warning restore RS0026, RS0027
+
+        /// <summary>
+        /// Execute an arbitrary database-specific command against the server; this is primarily intended for
+        /// executing modules, but may also be used to provide access to new features that lack
+        /// a direct API.
+        /// </summary>
+        /// <param name="database">The database ID; if <see langword="null"/>, the configured default database is used.</param>
+        /// <param name="command">The command to run.</param>
+        /// <param name="args">The arguments to pass for the command.</param>
+        /// <param name="flags">The flags to use for this operation.</param>
+        /// <returns>A dynamic representation of the command's result.</returns>
+        /// <remarks>This API should be considered an advanced feature; inappropriate use can be harmful.</remarks>
+        RedisResult Execute(int? database, string command, ICollection<object> args, CommandFlags flags = CommandFlags.None);
+
+        /// <inheritdoc cref="Execute(int?, string, ICollection{object}, CommandFlags)"/>
+        Task<RedisResult> ExecuteAsync(int? database, string command, ICollection<object> args, CommandFlags flags = CommandFlags.None);
 
         /// <summary>
         /// Delete all the keys of all databases on the server.
---
+++ tests/StackExchange.Redis.Tests/GetServerTests.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StackExchange.Redis.Tests;
+
+public abstract class GetServerTestsBase(ITestOutputHelper output, SharedConnectionFixture fixture)
+    : TestBase(output, fixture)
+{
+    protected abstract bool IsCluster { get; }
+
+    [Fact]
+    public async Task GetServersMemoization()
+    {
+        await using var conn = Create();
+
+        var servers0 = conn.GetServers();
+        var servers1 = conn.GetServers();
+
+        // different array, exact same contents
+        Assert.NotSame(servers0, servers1);
+        Assert.NotEmpty(servers0);
+        Assert.NotNull(servers0);
+        Assert.NotNull(servers1);
+        Assert.Equal(servers0.Length, servers1.Length);
+        for (int i = 0; i < servers0.Length; i++)
+        {
+            Assert.Same(servers0[i], servers1[i]);
+        }
+    }
+
+    [Fact]
+    public async Task GetServerByEndpointMemoization()
+    {
+        await using var conn = Create();
+        var ep = conn.GetEndPoints().First();
+
+        IServer x = conn.GetServer(ep), y = conn.GetServer(ep);
+        Assert.Same(x, y);
+
+        object asyncState = "whatever";
+        x = conn.GetServer(ep, asyncState);
+        y = conn.GetServer(ep, asyncState);
+        Assert.NotSame(x, y);
+    }
+
+    [Fact]
+    public async Task GetServerByKeyMemoization()
+    {
+        await using var conn = Create();
+        RedisKey key = Me();
+        string value = $"{key}:value";
+        await conn.GetDatabase().StringSetAsync(key, value);
+
+        IServer x = conn.GetServer(key), y = conn.GetServer(key);
+        Assert.False(y.IsReplica, "IsReplica");
+        Assert.Same(x, y);
+
+        y = conn.GetServer(key, flags: CommandFlags.DemandMaster);
+        Assert.Same(x, y);
+
+        // async state demands separate instance
+        y = conn.GetServer(key, "async state", flags: CommandFlags.DemandMaster);
+        Assert.NotSame(x, y);
+
+        // primary and replica should be different
+        y = conn.GetServer(key, flags: CommandFlags.DemandReplica);
+        Assert.NotSame(x, y);
+        Assert.True(y.IsReplica, "IsReplica");
+
+        // replica again: same
+        var z = conn.GetServer(key, flags: CommandFlags.DemandReplica);
+        Assert.Same(y, z);
+
+        // check routed correctly
+        var actual = (string?)await x.ExecuteAsync(null, "get", [key], CommandFlags.NoRedirect);
+        Assert.Equal(value, actual); // check value against primary
+
+        // for replica, don't check the value, because of replication delay - just: no error
+        _ = y.ExecuteAsync(null, "get", [key], CommandFlags.NoRedirect);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task GetServerWithDefaultKey(bool explicitNull)
+    {
+        await using var conn = Create();
+        bool isCluster = conn.ServerSelectionStrategy.ServerType == ServerType.Cluster;
+        Assert.Equal(IsCluster, isCluster); // check our assumptions!
+
+        // we expect explicit null and default to act the same, but: check
+        RedisKey key = explicitNull ? RedisKey.Null : default(RedisKey);
+
+        IServer primary = conn.GetServer(key);
+        Assert.False(primary.IsReplica);
+
+        IServer replica = conn.GetServer(key, flags: CommandFlags.DemandReplica);
+        Assert.True(replica.IsReplica);
+
+        // check multiple calls
+        HashSet<IServer> uniques = [];
+        for (int i = 0; i < 100; i++)
+        {
+            uniques.Add(conn.GetServer(key));
+        }
+
+        if (isCluster)
+        {
+            Assert.True(uniques.Count > 1); // should be able to get arbitrary servers
+        }
+        else
+        {
+            Assert.Single(uniques);
+        }
+
+        uniques.Clear();
+        for (int i = 0; i < 100; i++)
+        {
+            uniques.Add(conn.GetServer(key, flags: CommandFlags.DemandReplica));
+        }
+
+        if (isCluster)
+        {
+            Assert.True(uniques.Count > 1); // should be able to get arbitrary servers
+        }
+        else
+        {
+            Assert.Single(uniques);
+        }
+    }
+}
+
+[RunPerProtocol]
+public class GetServerTestsCluster(ITestOutputHelper output, SharedConnectionFixture fixture) : GetServerTestsBase(output, fixture)
+{
+    protected override string GetConfiguration() => TestConfig.Current.ClusterServersAndPorts;
+
+    protected override bool IsCluster => true;
+}
+
+[RunPerProtocol]
+public class GetServerTestsStandalone(ITestOutputHelper output, SharedConnectionFixture fixture) : GetServerTestsBase(output, fixture)
+{
+    protected override string GetConfiguration() => // we want to test flags usage including replicas
+        TestConfig.Current.PrimaryServerAndPort + "," + TestConfig.Current.ReplicaServerAndPort;
+
+    protected override bool IsCluster => false;
+}
