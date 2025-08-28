f87e3f85b61020bf3f3957fc0b0ce8d8221edc3e
Marc Gravell <marc.gravell@gmail.com>
2025-08-28 06:30:26 +0100

    let's try committing all the files

---
+++ src/RESPite.Redis/RedisExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using Resp;
+
+namespace RESPite.Redis;
+
+public static class RedisExtensions
+{
+    extension(in RespContext context)
+    {
+        // since this is valid...
+        // public ref readonly RespContext Self => ref context;
+
+        // so must this be (importantly, RedisStrings has only a single RespContext field)
+        public ref readonly RedisStrings Strings
+            => ref Unsafe.As<RespContext, RedisStrings>(ref Unsafe.AsRef(in context));
+
+        public ref readonly RedisKeys Keys
+            => ref Unsafe.As<RespContext, RedisKeys>(ref Unsafe.AsRef(in context));
+    }
+}
