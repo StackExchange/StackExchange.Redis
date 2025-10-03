630d6af592f8bccef93a52a754bb5ad172960a42
Marc Gravell <marc.gravell@gmail.com>
2025-10-03 16:11:58 +0100

    hyperloglog

--- src/RESPite.StackExchange.Redis/RespContextDatabase.HyperLogLog.cs
+++ src/RESPite.StackExchange.Redis/RespContextDatabase.HyperLogLog.cs
@@ -1,58 +1,79 @@
-﻿using StackExchange.Redis;
+﻿using RESPite.Messages;
+using StackExchange.Redis;
 
 namespace RESPite.StackExchange.Redis;
 
 internal partial class RespContextDatabase
 {
     // Async HyperLogLog methods
-    public Task<bool> HyperLogLogAddAsync(RedisKey key, RedisValue value, CommandFlags flags = CommandFlags.None) =>
-        throw new NotImplementedException();
+    public Task<bool> HyperLogLogAddAsync(
+        RedisKey key,
+        RedisValue value,
+        CommandFlags flags = CommandFlags.None) =>
+        Context(flags).HyperLogLogs().PfAdd(key, value).AsTask();
 
-    public Task<bool> HyperLogLogAddAsync(RedisKey key, RedisValue[] values, CommandFlags flags = CommandFlags.None) =>
-        throw new NotImplementedException();
+    public Task<bool> HyperLogLogAddAsync(
+        RedisKey key,
+        RedisValue[] values,
+        CommandFlags flags = CommandFlags.None) =>
+        Context(flags).HyperLogLogs().PfAdd(key, values).AsTask();
 
-    public Task<long> HyperLogLogLengthAsync(RedisKey key, CommandFlags flags = CommandFlags.None) =>
-        throw new NotImplementedException();
+    public Task<long> HyperLogLogLengthAsync(
+        RedisKey key,
+        CommandFlags flags = CommandFlags.None) =>
+        Context(flags).HyperLogLogs().PfCount(key).AsTask();
 
-    public Task<long> HyperLogLogLengthAsync(RedisKey[] keys, CommandFlags flags = CommandFlags.None) =>
-        throw new NotImplementedException();
+    public Task<long> HyperLogLogLengthAsync(
+        RedisKey[] keys,
+        CommandFlags flags = CommandFlags.None) =>
+        Context(flags).HyperLogLogs().PfCount(keys).AsTask();
 
     public Task HyperLogLogMergeAsync(
         RedisKey destination,
         RedisKey first,
         RedisKey second,
         CommandFlags flags = CommandFlags.None) =>
-        throw new NotImplementedException();
+        Context(flags).HyperLogLogs().PfMerge(destination, first, second).AsTask();
 
     public Task HyperLogLogMergeAsync(
         RedisKey destination,
         RedisKey[] sourceKeys,
         CommandFlags flags = CommandFlags.None) =>
-        throw new NotImplementedException();
+        Context(flags).HyperLogLogs().PfMerge(destination, sourceKeys).AsTask();
 
     // Synchronous HyperLogLog methods
-    public bool HyperLogLogAdd(RedisKey key, RedisValue value, CommandFlags flags = CommandFlags.None) =>
-        throw new NotImplementedException();
+    public bool HyperLogLogAdd(
+        RedisKey key,
+        RedisValue value,
+        CommandFlags flags = CommandFlags.None) =>
+        Context(flags).HyperLogLogs().PfAdd(key, value).Wait(SyncTimeout);
 
-    public bool HyperLogLogAdd(RedisKey key, RedisValue[] values, CommandFlags flags = CommandFlags.None) =>
-        throw new NotImplementedException();
+    public bool HyperLogLogAdd(
+        RedisKey key,
+        RedisValue[] values,
+        CommandFlags flags = CommandFlags.None) =>
+        Context(flags).HyperLogLogs().PfAdd(key, values).Wait(SyncTimeout);
 
-    public long HyperLogLogLength(RedisKey key, CommandFlags flags = CommandFlags.None) =>
-        throw new NotImplementedException();
+    public long HyperLogLogLength(
+        RedisKey key,
+        CommandFlags flags = CommandFlags.None) =>
+        Context(flags).HyperLogLogs().PfCount(key).Wait(SyncTimeout);
 
-    public long HyperLogLogLength(RedisKey[] keys, CommandFlags flags = CommandFlags.None) =>
-        throw new NotImplementedException();
+    public long HyperLogLogLength(
+        RedisKey[] keys,
+        CommandFlags flags = CommandFlags.None) =>
+        Context(flags).HyperLogLogs().PfCount(keys).Wait(SyncTimeout);
 
     public void HyperLogLogMerge(
         RedisKey destination,
         RedisKey first,
         RedisKey second,
         CommandFlags flags = CommandFlags.None) =>
-        throw new NotImplementedException();
+        Context(flags).HyperLogLogs().PfMerge(destination, first, second).Wait(SyncTimeout);
 
     public void HyperLogLogMerge(
         RedisKey destination,
         RedisKey[] sourceKeys,
         CommandFlags flags = CommandFlags.None) =>
-        throw new NotImplementedException();
+        Context(flags).HyperLogLogs().PfMerge(destination, sourceKeys).Wait(SyncTimeout);
 }
