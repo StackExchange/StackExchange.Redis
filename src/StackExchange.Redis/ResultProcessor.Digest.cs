59351140e5381f0722b246bbca80cc86a5429ecf
Marc Gravell <marc.gravell@gmail.com>
2025-11-03 12:30:11 +0000

    remove mitigations for receiving under-length digests from the server (now fixed at server)

--- src/StackExchange.Redis/ResultProcessor.Digest.cs
+++ src/StackExchange.Redis/ResultProcessor.Digest.cs
@@ -13,36 +13,28 @@ internal abstract partial class ResultProcessor
     {
         protected override bool SetResultCore(PhysicalConnection connection, Message message, in RawResult result)
         {
-            if (result.IsNull)
+            if (result.IsNull) // for example, key doesn't exist
             {
                 SetResult(message, null);
                 return true;
             }
 
-            switch (result.Resp2TypeBulkString)
+            if (result.Resp2TypeBulkString == ResultType.BulkString
+                && result.Payload is { Length: 2 * ValueCondition.DigestBytes } payload)
             {
-                case ResultType.BulkString:
-                    var payload = result.Payload;
-                    var len = checked((int)payload.Length);
-                    if (len == 2 * ValueCondition.DigestBytes & payload.IsSingleSegment)
-                    {
-                        // full-size hash in a single chunk - fast path
-                        SetResult(message, ValueCondition.ParseDigest(payload.First.Span));
-                        return true;
-                    }
-
-                    if (len >= 1 & len <= ValueCondition.DigestBytes * 2)
-                    {
-                        // Either multi-segment, or isn't long enough (missing leading zeros,
-                        // see https://github.com/redis/redis/issues/14496).
-                        Span<byte> buffer = new byte[2 * ValueCondition.DigestBytes];
-                        int start = (2 * ValueCondition.DigestBytes) - len;
-                        if (start != 0) buffer.Slice(0, start).Fill((byte)'0'); // pad
-                        payload.CopyTo(buffer.Slice(start)); // linearize
-                        SetResult(message, ValueCondition.ParseDigest(buffer));
-                        return true;
-                    }
-                    break;
+                ValueCondition digest;
+                if (payload.IsSingleSegment) // single chunk - fast path
+                {
+                    digest = ValueCondition.ParseDigest(payload.First.Span);
+                }
+                else // linearize
+                {
+                    Span<byte> buffer = stackalloc byte[2 * ValueCondition.DigestBytes];
+                    payload.CopyTo(buffer);
+                    digest = ValueCondition.ParseDigest(buffer);
+                }
+                SetResult(message, digest);
+                return true;
             }
             return false;
         }
