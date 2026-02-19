7d61495e1d0be24e2a55f3801f4584dbb0a59885
Marc Gravell <marc.gravell@gmail.com>
2026-02-19 14:20:52 +0000

    include all the files

---
+++ tests/StackExchange.Redis.Tests/ResultProcessorUnitTests/GeoRadius.cs
@@ -0,0 +1,141 @@
+﻿using Xunit;
+
+namespace StackExchange.Redis.Tests.ResultProcessorUnitTests;
+
+public class GeoRadius(ITestOutputHelper log) : ResultProcessorUnitTest(log)
+{
+    [Fact]
+    public void GeoRadius_None_ReturnsJustMembers()
+    {
+        // Without any WITH option: just member names as scalars in array
+        var resp = "*2\r\n$7\r\nPalermo\r\n$7\r\nCatania\r\n";
+        var result = Execute<GeoRadiusResult[]>(resp, ResultProcessor.GeoRadiusArray(GeoRadiusOptions.None));
+
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Length);
+        Assert.Equal("Palermo", result[0].Member);
+        Assert.Null(result[0].Distance);
+        Assert.Null(result[0].Hash);
+        Assert.Null(result[0].Position);
+        Assert.Equal("Catania", result[1].Member);
+        Assert.Null(result[1].Distance);
+        Assert.Null(result[1].Hash);
+        Assert.Null(result[1].Position);
+    }
+
+    [Fact]
+    public void GeoRadius_WithDistance_ReturnsDistances()
+    {
+        // With WITHDIST: each element is [member, distance]
+        var resp = "*2\r\n" +
+                   "*2\r\n$7\r\nPalermo\r\n$8\r\n190.4424\r\n" +
+                   "*2\r\n$7\r\nCatania\r\n$7\r\n56.4413\r\n";
+        var result = Execute<GeoRadiusResult[]>(resp, ResultProcessor.GeoRadiusArray(GeoRadiusOptions.WithDistance));
+
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Length);
+        Assert.Equal("Palermo", result[0].Member);
+        Assert.Equal(190.4424, result[0].Distance);
+        Assert.Null(result[0].Hash);
+        Assert.Null(result[0].Position);
+        Assert.Equal("Catania", result[1].Member);
+        Assert.Equal(56.4413, result[1].Distance);
+        Assert.Null(result[1].Hash);
+        Assert.Null(result[1].Position);
+    }
+
+    [Fact]
+    public void GeoRadius_WithCoordinates_ReturnsPositions()
+    {
+        // With WITHCOORD: each element is [member, [longitude, latitude]]
+        var resp = "*2\r\n" +
+                   "*2\r\n$7\r\nPalermo\r\n*2\r\n$18\r\n13.361389338970184\r\n$16\r\n38.1155563954963\r\n" +
+                   "*2\r\n$7\r\nCatania\r\n*2\r\n$18\r\n15.087267458438873\r\n$17\r\n37.50266842333162\r\n";
+        var result = Execute<GeoRadiusResult[]>(resp, ResultProcessor.GeoRadiusArray(GeoRadiusOptions.WithCoordinates));
+
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Length);
+        Assert.Equal("Palermo", result[0].Member);
+        Assert.Null(result[0].Distance);
+        Assert.Null(result[0].Hash);
+        Assert.NotNull(result[0].Position);
+        Assert.Equal(13.361389338970184, result[0].Position!.Value.Longitude);
+        Assert.Equal(38.1155563954963, result[0].Position!.Value.Latitude);
+        Assert.Equal("Catania", result[1].Member);
+        Assert.Null(result[1].Distance);
+        Assert.Null(result[1].Hash);
+        Assert.NotNull(result[1].Position);
+        Assert.Equal(15.087267458438873, result[1].Position!.Value.Longitude);
+        Assert.Equal(37.50266842333162, result[1].Position!.Value.Latitude);
+    }
+
+    [Fact]
+    public void GeoRadius_WithDistanceAndCoordinates_ReturnsBoth()
+    {
+        // With WITHDIST WITHCOORD: each element is [member, distance, [longitude, latitude]]
+        var resp = "*2\r\n" +
+                   "*3\r\n$7\r\nPalermo\r\n$8\r\n190.4424\r\n*2\r\n$18\r\n13.361389338970184\r\n$16\r\n38.1155563954963\r\n" +
+                   "*3\r\n$7\r\nCatania\r\n$7\r\n56.4413\r\n*2\r\n$18\r\n15.087267458438873\r\n$17\r\n37.50266842333162\r\n";
+        var result = Execute<GeoRadiusResult[]>(resp, ResultProcessor.GeoRadiusArray(GeoRadiusOptions.WithDistance | GeoRadiusOptions.WithCoordinates));
+
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Length);
+        Assert.Equal("Palermo", result[0].Member);
+        Assert.Equal(190.4424, result[0].Distance);
+        Assert.Null(result[0].Hash);
+        Assert.NotNull(result[0].Position);
+        Assert.Equal(13.361389338970184, result[0].Position!.Value.Longitude);
+        Assert.Equal(38.1155563954963, result[0].Position!.Value.Latitude);
+    }
+
+    [Fact]
+    public void GeoRadius_WithHash_ReturnsHash()
+    {
+        // With WITHHASH: each element is [member, hash]
+        var resp = "*2\r\n" +
+                   "*2\r\n$7\r\nPalermo\r\n:3479099956230698\r\n" +
+                   "*2\r\n$7\r\nCatania\r\n:3479447370796909\r\n";
+        var result = Execute<GeoRadiusResult[]>(resp, ResultProcessor.GeoRadiusArray(GeoRadiusOptions.WithGeoHash));
+
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Length);
+        Assert.Equal("Palermo", result[0].Member);
+        Assert.Null(result[0].Distance);
+        Assert.Equal(3479099956230698, result[0].Hash);
+        Assert.Null(result[0].Position);
+        Assert.Equal("Catania", result[1].Member);
+        Assert.Null(result[1].Distance);
+        Assert.Equal(3479447370796909, result[1].Hash);
+        Assert.Null(result[1].Position);
+    }
+
+    [Fact]
+    public void GeoRadius_AllOptions_ReturnsEverything()
+    {
+        // With all options: [member, distance, hash, [longitude, latitude]]
+        var resp = "*1\r\n" +
+                   "*4\r\n$7\r\nPalermo\r\n$8\r\n190.4424\r\n:3479099956230698\r\n*2\r\n$18\r\n13.361389338970184\r\n$16\r\n38.1155563954963\r\n";
+        var result = Execute<GeoRadiusResult[]>(
+            resp,
+            ResultProcessor.GeoRadiusArray(GeoRadiusOptions.WithDistance | GeoRadiusOptions.WithGeoHash | GeoRadiusOptions.WithCoordinates));
+
+        Assert.NotNull(result);
+        Assert.Single(result);
+        Assert.Equal("Palermo", result[0].Member);
+        Assert.Equal(190.4424, result[0].Distance);
+        Assert.Equal(3479099956230698, result[0].Hash);
+        Assert.NotNull(result[0].Position);
+        Assert.Equal(13.361389338970184, result[0].Position!.Value.Longitude);
+        Assert.Equal(38.1155563954963, result[0].Position!.Value.Latitude);
+    }
+
+    [Fact]
+    public void GeoRadius_EmptyArray_ReturnsEmptyArray()
+    {
+        var resp = "*0\r\n";
+        var result = Execute<GeoRadiusResult[]>(resp, ResultProcessor.GeoRadiusArray(GeoRadiusOptions.None));
+
+        Assert.NotNull(result);
+        Assert.Empty(result);
+    }
+}
---
+++ tests/StackExchange.Redis.Tests/ResultProcessorUnitTests/VectorSet.cs
@@ -0,0 +1,81 @@
+using Xunit;
+
+namespace StackExchange.Redis.Tests.ResultProcessorUnitTests;
+
+public class VectorSet(ITestOutputHelper log) : ResultProcessorUnitTest(log)
+{
+    [Theory]
+    [InlineData("*0\r\n")] // empty array
+    [InlineData("*12\r\n$4\r\nsize\r\n:100\r\n$8\r\nvset-uid\r\n:42\r\n$9\r\nmax-level\r\n:5\r\n$10\r\nvector-dim\r\n:128\r\n$10\r\nquant-type\r\n$4\r\nint8\r\n$17\r\nhnsw-max-node-uid\r\n:99\r\n")] // full info with int8
+    public void VectorSetInfo_ValidInput(string resp)
+    {
+        var processor = ResultProcessor.VectorSetInfo;
+        var result = Execute(resp, processor);
+
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public void VectorSetInfo_EmptyArray_ReturnsDefaults()
+    {
+        // Empty array should return VectorSetInfo with default values
+        var resp = "*0\r\n";
+        var processor = ResultProcessor.VectorSetInfo;
+        var result = Execute(resp, processor);
+
+        Assert.NotNull(result);
+        Assert.Equal(VectorSetQuantization.Unknown, result.Value.Quantization);
+        Assert.Null(result.Value.QuantizationRaw);
+        Assert.Equal(0, result.Value.Dimension);
+        Assert.Equal(0, result.Value.Length);
+        Assert.Equal(0, result.Value.MaxLevel);
+        Assert.Equal(0, result.Value.VectorSetUid);
+        Assert.Equal(0, result.Value.HnswMaxNodeUid);
+    }
+
+    [Theory]
+    [InlineData("*-1\r\n")] // null array (RESP2)
+    [InlineData("_\r\n")] // null (RESP3)
+    public void VectorSetInfo_NullArray(string resp)
+    {
+        var processor = ResultProcessor.VectorSetInfo;
+        var result = Execute(resp, processor);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void VectorSetInfo_ValidatesContent_Int8()
+    {
+        // VINFO response with int8 quantization
+        var resp = "*12\r\n$4\r\nsize\r\n:100\r\n$8\r\nvset-uid\r\n:42\r\n$9\r\nmax-level\r\n:5\r\n$10\r\nvector-dim\r\n:128\r\n$10\r\nquant-type\r\n$4\r\nint8\r\n$17\r\nhnsw-max-node-uid\r\n:99\r\n";
+        var processor = ResultProcessor.VectorSetInfo;
+        var result = Execute(resp, processor);
+
+        Assert.NotNull(result);
+        Assert.Equal(VectorSetQuantization.Int8, result.Value.Quantization);
+        Assert.Null(result.Value.QuantizationRaw);
+        Assert.Equal(128, result.Value.Dimension);
+        Assert.Equal(100, result.Value.Length);
+        Assert.Equal(5, result.Value.MaxLevel);
+        Assert.Equal(42, result.Value.VectorSetUid);
+        Assert.Equal(99, result.Value.HnswMaxNodeUid);
+    }
+
+    [Fact]
+    public void VectorSetInfo_SkipsNonScalarValues()
+    {
+        // Response with a non-scalar value (array) that should be skipped
+        // Format: size:100, unknown-field:[1,2,3], vset-uid:42
+        var resp = "*6\r\n$4\r\nsize\r\n:100\r\n$13\r\nunknown-field\r\n*3\r\n:1\r\n:2\r\n:3\r\n$8\r\nvset-uid\r\n:42\r\n";
+        var processor = ResultProcessor.VectorSetInfo;
+        var result = Execute(resp, processor);
+
+        Assert.NotNull(result);
+        Assert.Equal(100, result.Value.Length);
+        Assert.Equal(42, result.Value.VectorSetUid);
+        // Other fields should have default values
+        Assert.Equal(VectorSetQuantization.Unknown, result.Value.Quantization);
+        Assert.Equal(0, result.Value.Dimension);
+    }
+}
