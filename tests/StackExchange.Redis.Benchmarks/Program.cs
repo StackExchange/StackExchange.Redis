ba1f6c12e2ce1482e4ead29f655e71f2c4fbac70
Marc Gravell <marc.gravell@gmail.com>
2026-02-22 15:00:34 +0000

    enum parse benchmark

--- tests/StackExchange.Redis.Benchmarks/Program.cs
+++ tests/StackExchange.Redis.Benchmarks/Program.cs
@@ -18,6 +18,17 @@ namespace StackExchange.Redis.Benchmarks
                 obj.HashCS_B();
                 obj.HashCS_C();
             }
+
+            var obj2 = new EnumParseBenchmarks();
+            foreach (var value in obj2.Values())
+            {
+                Console.WriteLine($"Value: {value}");
+                obj2.Value = value;
+                // obj2.Setup();
+                Console.WriteLine($"  Enum: {obj2.EnumParse_CS()}");
+                Console.WriteLine($"  Fast: {obj2.FastHash_CS()}");
+                Console.WriteLine();
+            }
 #else
             BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
 #endif
