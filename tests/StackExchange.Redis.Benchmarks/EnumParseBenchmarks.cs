f7e9d59fe08b7798dfcdbce352f8381a2e3d9c78
Marc Gravell <marc.gravell@gmail.com>
2026-02-22 15:58:22 +0000

    more enum bench

--- tests/StackExchange.Redis.Benchmarks/EnumParseBenchmarks.cs
+++ tests/StackExchange.Redis.Benchmarks/EnumParseBenchmarks.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using BenchmarkDotNet.Attributes;
 using RESPite;
 
@@ -19,35 +20,336 @@ public partial class EnumParseBenchmarks
         "expireat",
         "zremrangebyscore"
     ];
+
+    private byte[] _bytes = [];
+
     [ParamsSource(nameof(Values))]
-    public string Value { get; set; } = "";
+    public string Value
+    {
+        get;
+        set
+        {
+            _bytes = Encoding.UTF8.GetBytes(value);
+            field = value;
+        }
+#pragma warning disable SA1500
+    } = "";
+#pragma warning restore SA1500
 
     [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
     public RedisCommand EnumParse_CS()
     {
-        var s = Value;
+        var value = Value;
         RedisCommand r = default;
         for (int i = 0; i < OperationsPerInvoke; i++)
         {
-            Enum.TryParse(s, out r);
+            Enum.TryParse(value, out r);
         }
+
         return r;
     }
 
     [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
     public RedisCommand FastHash_CS()
     {
-        var s = Value;
+        ReadOnlySpan<char> value = Value;
         RedisCommand r = default;
         for (int i = 0; i < OperationsPerInvoke; i++)
         {
-            TryParse_CS(s, out r);
+            TryParse_CS(value, out r);
         }
+
         return r;
     }
 
+    [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
+    public RedisCommand Bytes_CS()
+    {
+        ReadOnlySpan<byte> value = _bytes;
+        RedisCommand r = default;
+        for (int i = 0; i < OperationsPerInvoke; i++)
+        {
+            TryParse_CS(value, out r);
+        }
+
+        return r;
+    }
+
+    [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
+    public RedisCommand Switch()
+    {
+        var value = Value;
+        RedisCommand r = default;
+        for (int i = 0; i < OperationsPerInvoke; i++)
+        {
+            TryParseSwitch(value, out r);
+        }
+
+        return r;
+    }
+
+    private static bool TryParseSwitch(string s, out RedisCommand r)
+    {
+        r = s switch
+        {
+            "NONE" => RedisCommand.NONE,
+            "APPEND" => RedisCommand.APPEND,
+            "ASKING" => RedisCommand.ASKING,
+            "AUTH" => RedisCommand.AUTH,
+            "BGREWRITEAOF" => RedisCommand.BGREWRITEAOF,
+            "BGSAVE" => RedisCommand.BGSAVE,
+            "BITCOUNT" => RedisCommand.BITCOUNT,
+            "BITOP" => RedisCommand.BITOP,
+            "BITPOS" => RedisCommand.BITPOS,
+            "BLPOP" => RedisCommand.BLPOP,
+            "BRPOP" => RedisCommand.BRPOP,
+            "BRPOPLPUSH" => RedisCommand.BRPOPLPUSH,
+            "CLIENT" => RedisCommand.CLIENT,
+            "CLUSTER" => RedisCommand.CLUSTER,
+            "CONFIG" => RedisCommand.CONFIG,
+            "COPY" => RedisCommand.COPY,
+            "COMMAND" => RedisCommand.COMMAND,
+            "DBSIZE" => RedisCommand.DBSIZE,
+            "DEBUG" => RedisCommand.DEBUG,
+            "DECR" => RedisCommand.DECR,
+            "DECRBY" => RedisCommand.DECRBY,
+            "DEL" => RedisCommand.DEL,
+            "DELEX" => RedisCommand.DELEX,
+            "DIGEST" => RedisCommand.DIGEST,
+            "DISCARD" => RedisCommand.DISCARD,
+            "DUMP" => RedisCommand.DUMP,
+            "ECHO" => RedisCommand.ECHO,
+            "EVAL" => RedisCommand.EVAL,
+            "EVALSHA" => RedisCommand.EVALSHA,
+            "EVAL_RO" => RedisCommand.EVAL_RO,
+            "EVALSHA_RO" => RedisCommand.EVALSHA_RO,
+            "EXEC" => RedisCommand.EXEC,
+            "EXISTS" => RedisCommand.EXISTS,
+            "EXPIRE" => RedisCommand.EXPIRE,
+            "EXPIREAT" => RedisCommand.EXPIREAT,
+            "EXPIRETIME" => RedisCommand.EXPIRETIME,
+            "FLUSHALL" => RedisCommand.FLUSHALL,
+            "FLUSHDB" => RedisCommand.FLUSHDB,
+            "GEOADD" => RedisCommand.GEOADD,
+            "GEODIST" => RedisCommand.GEODIST,
+            "GEOHASH" => RedisCommand.GEOHASH,
+            "GEOPOS" => RedisCommand.GEOPOS,
+            "GEORADIUS" => RedisCommand.GEORADIUS,
+            "GEORADIUSBYMEMBER" => RedisCommand.GEORADIUSBYMEMBER,
+            "GEOSEARCH" => RedisCommand.GEOSEARCH,
+            "GEOSEARCHSTORE" => RedisCommand.GEOSEARCHSTORE,
+            "GET" => RedisCommand.GET,
+            "GETBIT" => RedisCommand.GETBIT,
+            "GETDEL" => RedisCommand.GETDEL,
+            "GETEX" => RedisCommand.GETEX,
+            "GETRANGE" => RedisCommand.GETRANGE,
+            "GETSET" => RedisCommand.GETSET,
+            "HDEL" => RedisCommand.HDEL,
+            "HELLO" => RedisCommand.HELLO,
+            "HEXISTS" => RedisCommand.HEXISTS,
+            "HEXPIRE" => RedisCommand.HEXPIRE,
+            "HEXPIREAT" => RedisCommand.HEXPIREAT,
+            "HEXPIRETIME" => RedisCommand.HEXPIRETIME,
+            "HGET" => RedisCommand.HGET,
+            "HGETEX" => RedisCommand.HGETEX,
+            "HGETDEL" => RedisCommand.HGETDEL,
+            "HGETALL" => RedisCommand.HGETALL,
+            "HINCRBY" => RedisCommand.HINCRBY,
+            "HINCRBYFLOAT" => RedisCommand.HINCRBYFLOAT,
+            "HKEYS" => RedisCommand.HKEYS,
+            "HLEN" => RedisCommand.HLEN,
+            "HMGET" => RedisCommand.HMGET,
+            "HMSET" => RedisCommand.HMSET,
+            "HOTKEYS" => RedisCommand.HOTKEYS,
+            "HPERSIST" => RedisCommand.HPERSIST,
+            "HPEXPIRE" => RedisCommand.HPEXPIRE,
+            "HPEXPIREAT" => RedisCommand.HPEXPIREAT,
+            "HPEXPIRETIME" => RedisCommand.HPEXPIRETIME,
+            "HPTTL" => RedisCommand.HPTTL,
+            "HRANDFIELD" => RedisCommand.HRANDFIELD,
+            "HSCAN" => RedisCommand.HSCAN,
+            "HSET" => RedisCommand.HSET,
+            "HSETEX" => RedisCommand.HSETEX,
+            "HSETNX" => RedisCommand.HSETNX,
+            "HSTRLEN" => RedisCommand.HSTRLEN,
+            "HVALS" => RedisCommand.HVALS,
+            "INCR" => RedisCommand.INCR,
+            "INCRBY" => RedisCommand.INCRBY,
+            "INCRBYFLOAT" => RedisCommand.INCRBYFLOAT,
+            "INFO" => RedisCommand.INFO,
+            "KEYS" => RedisCommand.KEYS,
+            "LASTSAVE" => RedisCommand.LASTSAVE,
+            "LATENCY" => RedisCommand.LATENCY,
+            "LCS" => RedisCommand.LCS,
+            "LINDEX" => RedisCommand.LINDEX,
+            "LINSERT" => RedisCommand.LINSERT,
+            "LLEN" => RedisCommand.LLEN,
+            "LMOVE" => RedisCommand.LMOVE,
+            "LMPOP" => RedisCommand.LMPOP,
+            "LPOP" => RedisCommand.LPOP,
+            "LPOS" => RedisCommand.LPOS,
+            "LPUSH" => RedisCommand.LPUSH,
+            "LPUSHX" => RedisCommand.LPUSHX,
+            "LRANGE" => RedisCommand.LRANGE,
+            "LREM" => RedisCommand.LREM,
+            "LSET" => RedisCommand.LSET,
+            "LTRIM" => RedisCommand.LTRIM,
+            "MEMORY" => RedisCommand.MEMORY,
+            "MGET" => RedisCommand.MGET,
+            "MIGRATE" => RedisCommand.MIGRATE,
+            "MONITOR" => RedisCommand.MONITOR,
+            "MOVE" => RedisCommand.MOVE,
+            "MSET" => RedisCommand.MSET,
+            "MSETEX" => RedisCommand.MSETEX,
+            "MSETNX" => RedisCommand.MSETNX,
+            "MULTI" => RedisCommand.MULTI,
+            "OBJECT" => RedisCommand.OBJECT,
+            "PERSIST" => RedisCommand.PERSIST,
+            "PEXPIRE" => RedisCommand.PEXPIRE,
+            "PEXPIREAT" => RedisCommand.PEXPIREAT,
+            "PEXPIRETIME" => RedisCommand.PEXPIRETIME,
+            "PFADD" => RedisCommand.PFADD,
+            "PFCOUNT" => RedisCommand.PFCOUNT,
+            "PFMERGE" => RedisCommand.PFMERGE,
+            "PING" => RedisCommand.PING,
+            "PSETEX" => RedisCommand.PSETEX,
+            "PSUBSCRIBE" => RedisCommand.PSUBSCRIBE,
+            "PTTL" => RedisCommand.PTTL,
+            "PUBLISH" => RedisCommand.PUBLISH,
+            "PUBSUB" => RedisCommand.PUBSUB,
+            "PUNSUBSCRIBE" => RedisCommand.PUNSUBSCRIBE,
+            "QUIT" => RedisCommand.QUIT,
+            "RANDOMKEY" => RedisCommand.RANDOMKEY,
+            "READONLY" => RedisCommand.READONLY,
+            "READWRITE" => RedisCommand.READWRITE,
+            "RENAME" => RedisCommand.RENAME,
+            "RENAMENX" => RedisCommand.RENAMENX,
+            "REPLICAOF" => RedisCommand.REPLICAOF,
+            "RESTORE" => RedisCommand.RESTORE,
+            "ROLE" => RedisCommand.ROLE,
+            "RPOP" => RedisCommand.RPOP,
+            "RPOPLPUSH" => RedisCommand.RPOPLPUSH,
+            "RPUSH" => RedisCommand.RPUSH,
+            "RPUSHX" => RedisCommand.RPUSHX,
+            "SADD" => RedisCommand.SADD,
+            "SAVE" => RedisCommand.SAVE,
+            "SCAN" => RedisCommand.SCAN,
+            "SCARD" => RedisCommand.SCARD,
+            "SCRIPT" => RedisCommand.SCRIPT,
+            "SDIFF" => RedisCommand.SDIFF,
+            "SDIFFSTORE" => RedisCommand.SDIFFSTORE,
+            "SELECT" => RedisCommand.SELECT,
+            "SENTINEL" => RedisCommand.SENTINEL,
+            "SET" => RedisCommand.SET,
+            "SETBIT" => RedisCommand.SETBIT,
+            "SETEX" => RedisCommand.SETEX,
+            "SETNX" => RedisCommand.SETNX,
+            "SETRANGE" => RedisCommand.SETRANGE,
+            "SHUTDOWN" => RedisCommand.SHUTDOWN,
+            "SINTER" => RedisCommand.SINTER,
+            "SINTERCARD" => RedisCommand.SINTERCARD,
+            "SINTERSTORE" => RedisCommand.SINTERSTORE,
+            "SISMEMBER" => RedisCommand.SISMEMBER,
+            "SLAVEOF" => RedisCommand.SLAVEOF,
+            "SLOWLOG" => RedisCommand.SLOWLOG,
+            "SMEMBERS" => RedisCommand.SMEMBERS,
+            "SMISMEMBER" => RedisCommand.SMISMEMBER,
+            "SMOVE" => RedisCommand.SMOVE,
+            "SORT" => RedisCommand.SORT,
+            "SORT_RO" => RedisCommand.SORT_RO,
+            "SPOP" => RedisCommand.SPOP,
+            "SPUBLISH" => RedisCommand.SPUBLISH,
+            "SRANDMEMBER" => RedisCommand.SRANDMEMBER,
+            "SREM" => RedisCommand.SREM,
+            "STRLEN" => RedisCommand.STRLEN,
+            "SUBSCRIBE" => RedisCommand.SUBSCRIBE,
+            "SUNION" => RedisCommand.SUNION,
+            "SUNIONSTORE" => RedisCommand.SUNIONSTORE,
+            "SSCAN" => RedisCommand.SSCAN,
+            "SSUBSCRIBE" => RedisCommand.SSUBSCRIBE,
+            "SUNSUBSCRIBE" => RedisCommand.SUNSUBSCRIBE,
+            "SWAPDB" => RedisCommand.SWAPDB,
+            "SYNC" => RedisCommand.SYNC,
+            "TIME" => RedisCommand.TIME,
+            "TOUCH" => RedisCommand.TOUCH,
+            "TTL" => RedisCommand.TTL,
+            "TYPE" => RedisCommand.TYPE,
+            "UNLINK" => RedisCommand.UNLINK,
+            "UNSUBSCRIBE" => RedisCommand.UNSUBSCRIBE,
+            "UNWATCH" => RedisCommand.UNWATCH,
+            "VADD" => RedisCommand.VADD,
+            "VCARD" => RedisCommand.VCARD,
+            "VDIM" => RedisCommand.VDIM,
+            "VEMB" => RedisCommand.VEMB,
+            "VGETATTR" => RedisCommand.VGETATTR,
+            "VINFO" => RedisCommand.VINFO,
+            "VISMEMBER" => RedisCommand.VISMEMBER,
+            "VLINKS" => RedisCommand.VLINKS,
+            "VRANDMEMBER" => RedisCommand.VRANDMEMBER,
+            "VREM" => RedisCommand.VREM,
+            "VSETATTR" => RedisCommand.VSETATTR,
+            "VSIM" => RedisCommand.VSIM,
+            "WATCH" => RedisCommand.WATCH,
+            "XACK" => RedisCommand.XACK,
+            "XACKDEL" => RedisCommand.XACKDEL,
+            "XADD" => RedisCommand.XADD,
+            "XAUTOCLAIM" => RedisCommand.XAUTOCLAIM,
+            "XCLAIM" => RedisCommand.XCLAIM,
+            "XCFGSET" => RedisCommand.XCFGSET,
+            "XDEL" => RedisCommand.XDEL,
+            "XDELEX" => RedisCommand.XDELEX,
+            "XGROUP" => RedisCommand.XGROUP,
+            "XINFO" => RedisCommand.XINFO,
+            "XLEN" => RedisCommand.XLEN,
+            "XPENDING" => RedisCommand.XPENDING,
+            "XRANGE" => RedisCommand.XRANGE,
+            "XREAD" => RedisCommand.XREAD,
+            "XREADGROUP" => RedisCommand.XREADGROUP,
+            "XREVRANGE" => RedisCommand.XREVRANGE,
+            "XTRIM" => RedisCommand.XTRIM,
+            "ZADD" => RedisCommand.ZADD,
+            "ZCARD" => RedisCommand.ZCARD,
+            "ZCOUNT" => RedisCommand.ZCOUNT,
+            "ZDIFF" => RedisCommand.ZDIFF,
+            "ZDIFFSTORE" => RedisCommand.ZDIFFSTORE,
+            "ZINCRBY" => RedisCommand.ZINCRBY,
+            "ZINTER" => RedisCommand.ZINTER,
+            "ZINTERCARD" => RedisCommand.ZINTERCARD,
+            "ZINTERSTORE" => RedisCommand.ZINTERSTORE,
+            "ZLEXCOUNT" => RedisCommand.ZLEXCOUNT,
+            "ZMPOP" => RedisCommand.ZMPOP,
+            "ZMSCORE" => RedisCommand.ZMSCORE,
+            "ZPOPMAX" => RedisCommand.ZPOPMAX,
+            "ZPOPMIN" => RedisCommand.ZPOPMIN,
+            "ZRANDMEMBER" => RedisCommand.ZRANDMEMBER,
+            "ZRANGE" => RedisCommand.ZRANGE,
+            "ZRANGEBYLEX" => RedisCommand.ZRANGEBYLEX,
+            "ZRANGEBYSCORE" => RedisCommand.ZRANGEBYSCORE,
+            "ZRANGESTORE" => RedisCommand.ZRANGESTORE,
+            "ZRANK" => RedisCommand.ZRANK,
+            "ZREM" => RedisCommand.ZREM,
+            "ZREMRANGEBYLEX" => RedisCommand.ZREMRANGEBYLEX,
+            "ZREMRANGEBYRANK" => RedisCommand.ZREMRANGEBYRANK,
+            "ZREMRANGEBYSCORE" => RedisCommand.ZREMRANGEBYSCORE,
+            "ZREVRANGE" => RedisCommand.ZREVRANGE,
+            "ZREVRANGEBYLEX" => RedisCommand.ZREVRANGEBYLEX,
+            "ZREVRANGEBYSCORE" => RedisCommand.ZREVRANGEBYSCORE,
+            "ZREVRANK" => RedisCommand.ZREVRANK,
+            "ZSCAN" => RedisCommand.ZSCAN,
+            "ZSCORE" => RedisCommand.ZSCORE,
+            "ZUNION" => RedisCommand.ZUNION,
+            "ZUNIONSTORE" => RedisCommand.ZUNIONSTORE,
+            "UNKNOWN" => RedisCommand.UNKNOWN,
+            _ => (RedisCommand)(-1),
+        };
+        return r != (RedisCommand)(-1);
+    }
+
     [FastHash]
-    internal static partial bool TryParse_CS(string s, out RedisCommand r);
+    internal static partial bool TryParse_CS(ReadOnlySpan<char> value, out RedisCommand command);
+
+    [FastHash]
+    internal static partial bool TryParse_CS(ReadOnlySpan<byte> value, out RedisCommand command);
 
     public enum RedisCommand
     {
