9c92a654a03c3548c81043a1fe7538b97af3c019
Marc Gravell <marc.gravell@gmail.com>
2026-03-16 12:46:12 +0000

    wip

---
+++ src/RESPite/Shared/FrameworkShims.Sockets.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System.Net.Sockets;
+
+internal static class SocketExtensions
+{
+#if !NET
+    internal static ValueTask ConnectAsync(this Socket socket, EndPoint remoteEP, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static ValueTask<int> SendAsync(this Socket socket, ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static ValueTask<int> ReceiveAsync(this Socket socket, ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+#endif
+}
