110fc63db8c00ac102180b6912c65772dc3b145e
Marc Gravell <marc.gravell@gmail.com>
2026-05-24 11:42:37 +0100

    investigation into socket failure

---
+++ tests/StackExchange.Redis.Tests/TcpKeepAliveTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StackExchange.Redis.Tests;
+
+public class TcpKeepAliveTests(ITestOutputHelper log, TcpTestFixture fixture) : IClassFixture<TcpTestFixture>
+{
+    [Theory]
+    [InlineData(true, true)]
+    [InlineData(true, false)]
+    [InlineData(false, true)]
+    [InlineData(false, false)]
+    public async Task Roundtrip(bool ip, bool keepAlive)
+    {
+        using var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        EndPoint ep = ip ? fixture.IP : fixture.Dns;
+        log.WriteLine($"Connecting to {Format.ToString(ep)}, keepAlive: {keepAlive}");
+        if (keepAlive)
+        {
+            client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+        }
+        await client.ConnectAsync(ep);
+
+        byte[] buffer = new byte[4];
+        int i = random.Next(int.MinValue, int.MaxValue);
+        BinaryPrimitives.WriteInt32LittleEndian(buffer, i);
+        client.Send(buffer, 0, 4, SocketFlags.None);
+        Array.Clear(buffer, 0, buffer.Length);
+        Assert.Equal(0, BinaryPrimitives.ReadInt32LittleEndian(buffer));
+        int bytesRead, count = 0;
+        while (count < buffer.Length &&
+               (bytesRead = client.Receive(buffer, count, buffer.Length - count, SocketFlags.None)) > 0)
+        {
+            count += bytesRead;
+        }
+        if (count != buffer.Length) throw new EndOfStreamException();
+        Assert.Equal(i, BinaryPrimitives.ReadInt32LittleEndian(buffer));
+    }
+    private static readonly Random random = new();
+}
+
+public class TcpTestFixture : IDisposable
+{
+    private TcpListener server;
+    private CancellationTokenSource cts = new();
+    public IPEndPoint IP { get; }
+    public DnsEndPoint Dns { get; }
+
+    public TcpTestFixture()
+    {
+        int port = 18000;
+#if NET10_OR_GREATER
+        port += 1;
+#elif NET8_0_OR_GREATER
+        port += 2;
+#elif NET6_0_OR_GREATER
+        port += 3;
+#endif
+        var ip = IPAddress.Parse("172.24.32.1");
+        IP = new(ip, port);
+        Dns = new("marc-z13", port);
+        server = new TcpListener(ip, port);
+        server.Start();
+        _ = Task.Run(async () =>
+        {
+            try
+            {
+                while (!cts.IsCancellationRequested)
+                {
+                    #if NET
+                    var client = await server.AcceptTcpClientAsync(cts.Token);
+                    #else
+                    var client = await server.AcceptTcpClientAsync();
+                    #endif
+                    _ = Task.Run(() => RunClient(client, cts.Token));
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        });
+    }
+
+    private static async Task? RunClient(TcpClient client, CancellationToken cancel)
+    {
+        // echo up to 4 bytes
+        try
+        {
+            using var stream = client.GetStream();
+            byte[] buffer = new byte[4];
+            int bytesRead, count = 0;
+            while (count < buffer.Length &&
+                   (bytesRead = await stream.ReadAsync(buffer, count, buffer.Length - count, cancel)) > 0)
+            {
+                await stream.WriteAsync(buffer, count, bytesRead, cancel);
+                count += bytesRead;
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+        }
+        finally
+        {
+            client.Dispose();
+        }
+    }
+
+    public void Dispose()
+    {
+        cts.Cancel();
+        server.Stop();
+        #if NET
+        server.Dispose();
+        #endif
+    }
+}
