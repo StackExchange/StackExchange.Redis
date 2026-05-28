c02c1dfefbfc43d1f6f649691c1662a5867953a6
Marc Gravell <marc.gravell@gmail.com>
2026-05-28 11:51:45 +0100

    "classic"

--- src/RESPite.Benchmark/OldCoreBenchmark.cs
+++ src/RESPite.Benchmark/OldCoreBenchmark.cs
@@ -6,7 +6,7 @@ namespace RESPite.Benchmark;
 
 public sealed class OldCoreBenchmark(string[] args) : OldCoreBenchmarkBase(args)
 {
-    private static readonly string withVersion = $"legacy SE.Redis {GetLibVersion()}";
+    private static readonly string withVersion = $"classic SE.Redis {GetLibVersion()}";
     public override string ToString() => withVersion;
 
     protected override IConnectionMultiplexer Create(int port)
