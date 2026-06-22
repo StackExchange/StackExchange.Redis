329abde955630d70b47a5223a18c8128b0a0b83b
Ivan Tikhonov <pairbit@gmail.com>
2026-06-22 17:52:22 +0300

    add ResponseArrayPool (#3104)
    * add ResponseArrayPool
    
    * Lease
    
    * IMemoryOwner<byte>?
    
    ---------
    
    Co-authored-by: ITikhonov <ITikhonov@lanit.ru>

--- src/StackExchange.Redis/Lease.cs
+++ src/StackExchange.Redis/Lease.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Buffers;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace StackExchange.Redis
@@ -16,7 +17,7 @@ namespace StackExchange.Redis
         /// </summary>
         public static Lease<T> Empty { get; } = new Lease<T>(System.Array.Empty<T>(), 0);
 
-        private T[]? _arr;
+        private object? _buffer;
 
         /// <summary>
         /// Gets whether this lease is empty.
@@ -41,9 +42,29 @@ namespace StackExchange.Redis
             return new Lease<T>(arr, length);
         }
 
+        /// <summary>
+        /// Create a new lease.
+        /// </summary>
+        /// <param name="length">The size required.</param>
+        /// <param name="memoryOwner">Buffer.</param>
+        public static Lease<T> Create(int length, IMemoryOwner<T> memoryOwner)
+        {
+            if (length == 0) return Empty;
+            if ((uint)length > memoryOwner.Memory.Length)
+                throw new ArgumentOutOfRangeException(nameof(length));
+
+            return new Lease<T>(memoryOwner, length);
+        }
+
         private Lease(T[] arr, int length)
         {
-            _arr = arr;
+            _buffer = arr;
+            Length = length;
+        }
+
+        private Lease(IMemoryOwner<T> memoryOwner, int length)
+        {
+            _buffer = memoryOwner;
             Length = length;
         }
 
@@ -54,33 +75,50 @@ namespace StackExchange.Redis
         {
             if (Length != 0)
             {
-                var arr = Interlocked.Exchange(ref _arr, null);
-                if (arr != null) ArrayPool<T>.Shared.Return(arr);
+                var buffer = Interlocked.Exchange(ref _buffer, null);
+                if (buffer != null)
+                {
+                    if (buffer is T[] arr)
+                        ArrayPool<T>.Shared.Return(arr);
+                    else
+                        ((IMemoryOwner<T>)buffer).Dispose();
+                }
             }
         }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
         private static T[] ThrowDisposed() => throw new ObjectDisposedException(nameof(Lease<T>));
 
-        private T[] Array
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => _arr ?? ThrowDisposed();
-        }
-
         /// <summary>
         /// The data as a <see cref="Memory{T}"/>.
         /// </summary>
-        public Memory<T> Memory => new Memory<T>(Array, 0, Length);
+        public Memory<T> Memory => _buffer is IMemoryOwner<T> memoryOwner
+            ? memoryOwner.Memory.Slice(0, Length)
+            : new Memory<T>((T[]?)_buffer ?? ThrowDisposed(), 0, Length);
 
         /// <summary>
         /// The data as a <see cref="Span{T}"/>.
         /// </summary>
-        public Span<T> Span => new Span<T>(Array, 0, Length);
+        public Span<T> Span => _buffer is IMemoryOwner<T> memoryOwner
+            ? memoryOwner.Memory.Span.Slice(0, Length)
+            : new Span<T>((T[]?)_buffer ?? ThrowDisposed(), 0, Length);
 
         /// <summary>
         /// The data as an <see cref="ArraySegment{T}"/>.
         /// </summary>
-        public ArraySegment<T> ArraySegment => new ArraySegment<T>(Array, 0, Length);
+        public ArraySegment<T> ArraySegment
+        {
+            get
+            {
+                if (_buffer is IMemoryOwner<T> memoryOwner)
+                {
+                    if (!MemoryMarshal.TryGetArray((ReadOnlyMemory<T>)memoryOwner.Memory, out var segment))
+                        throw new NotSupportedException("Only array-backed buffers are supported");
+
+                    return new ArraySegment<T>(segment.Array!, segment.Offset, Length);
+                }
+                return new ArraySegment<T>((T[]?)_buffer ?? ThrowDisposed(), 0, Length);
+            }
+        }
     }
 }
